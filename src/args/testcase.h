3fccc2dcfda5911c14958861009e7846932fb011
eric <eric.schkufza@gmail.com>
2014-02-15 16:26:33 -0800

    Arg reader for testcases.

---
+++ src/args/testcase.h
@@ -0,0 +1,38 @@
+#ifndef STOKE_SRC_ARGS_TESTCASE_H
+#define STOKE_SRC_ARGS_TESTCASE_H
+
+#include <vector>
+
+#include "src/state/cpu_state.h"
+
+namespace cpputil {
+
+template <typename Style>
+struct TextReader<std::vector<CpuState>, Style> {
+	void operator()(std::istream& is, std::vector<CpuState>& ts) {
+		ts.clear();
+		for ( std::string s; !((is >> s).eof()); ) {
+			CpuState t;
+			is >> s >> t;
+			ts.emplace_back(t);
+		}
+		if ( is.eof() ) {
+			is.clear(ios::eofbit);
+		}	
+	}
+};
+
+template <typename Style>
+struct TextWriter<std::vector<CpuState>, Style> {
+	void operator()(std::ostream& os, const std::vector<CpuState>& ts) {
+		for ( size_t i = 0, ie = ts.size(); i < ie; ++i ) {
+			os << "Testcase " << i << ":" << std::endl << std::endl;
+			os << ts[i] << std::endl << std::endl;
+		}
+	}
+};
+
+} // namespace cpputil
+
+#endif
+
