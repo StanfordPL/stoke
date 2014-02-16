d234ff872ed733dea928bea1b5283086934eaf34
eric <eric.schkufza@gmail.com>
2014-02-15 16:16:50 -0800

    This wasn't checked in.

---
+++ src/args/args.h
@@ -0,0 +1,26 @@
+#ifndef STOKE_SRC_ARGS_ARGS_H
+#define STOKE_SRC_ARGS_ARGS_H
+
+#include "src/ext/cpputil/include/serialize/text_reader.h"
+#include "src/ext/cpputil/include/serialize/text_writer.h"
+#include "src/ext/x64asm/include/x64asm.h"
+
+namespace cpputil {
+
+template <typename Style>
+struct TextReader<x64asm::Code, Style> {
+	void operator()(std::istream& is, x64asm::Code& c) {
+		is >> c;
+	}
+};
+
+template <typename Style>
+struct TextWriter<x64asm::Code, Style> {
+	void operator()(std::ostream& os, const x64asm::Code& c) {
+		os << c;
+	}
+};
+
+} // namespace cpputil
+
+#endif
