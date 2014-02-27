89b48ad120889648d219ae06d0ad89f803928bf4
eric <eric.schkufza@gmail.com>
2014-02-26 21:48:19 -0800

    Line fixing in stoke extract.

--- tools/stoke_extract.cc
+++ tools/stoke_extract.cc
@@ -1,6 +1,8 @@
 #include <cctype>
 #include <cstdlib>
 
+#include <algorithm>
+#include <array>
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -41,17 +43,17 @@ bool exists(const string& file) {
 
 bool objdump(const string& file) {
   Terminal term;
-  term << "objdump -d -Msuffix " << file << " > /tmp/stoke.$USER.objdump >> /dev/null" << endl;
+  term << "objdump -d -Msuffix " << file << " > /tmp/stoke.$USER.objdump" << endl;
   return term.result() == 0;
 }
 
 bool mkdir() {
   Terminal term;
-  term << "mkdir -p " << out.value() << " >> /dev/null" << endl;
+  term << "mkdir -p " << out.value() << endl;
   return term.result() == 0;
 }
 
-void strip_header(ifstream& ifs) {
+void strip_prolog(ifstream& ifs) {
 	string s;
 	getline(ifs, s);
 	getline(ifs, s);
@@ -59,8 +61,7 @@ void strip_header(ifstream& ifs) {
 	getline(ifs, s);
 }
 
-bool ignore_section_header(ifstream& ifs, string& s) {
-	// Section headers begin with the word 'Displaying'
+bool ignore_header(ifstream& ifs, string& s) {
 	if ( s[0] == 'D' ) {
 		getline(ifs, s);
 		return true;
@@ -68,47 +69,55 @@ bool ignore_section_header(ifstream& ifs, string& s) {
 	return false;
 }
 
-string fxn_name(ifstream& ifs, string& s) {
-	// Function names are enclosed in angle brackets
+string get_name(ifstream& ifs, string& s) {
 	const auto begin = s.find_first_of('<') + 1;
 	const auto len = s.find_last_of('>') - begin;
 	return s.substr(begin, len);
 }
 
+string get_addr(const string& s) {
+	// Address is located between beginning of line and first :
+	auto begin = 0;
+	for ( ; isspace(s[begin]); ++begin);
+	const auto len = s.find_first_of(':') - begin;
+
+	return s.substr(begin, len);
+}
+
+bool get_instr(const string& s, string& instr) {
+	// Instructions begin after second tab; blank lines have only one
+	const auto tab1 = s.find_first_of('\t');
+	const auto tab2 = s.find_first_of('\t', tab1+1);
+	if ( tab2 == string::npos ) {
+		return false;
+	}
+	const auto begin = tab2 + 1;
+
+	// Instruction are terminated by eol, # or <
+	auto comment = s.find_last_of('#');
+	comment = comment == string::npos ? s.length() : comment;
+
+	auto annot = s.find_last_of('<');
+	annot = annot == string::npos ? s.length() : annot;
+
+	const auto len = min(comment, annot) - begin;
+	
+	instr = s.substr(begin, len);
+	return true;
+}
+
 line_map index_lines(ifstream& ifs, string& s) {
 	line_map lines;
 
+	// Functions are terminated by empty lines
 	while ( getline(ifs, s) ) {
-		// Functions are terminated by empty lines
 		if ( s.empty() ) {
 			break;
+		} 
+		string instr;
+		if ( get_instr(s, instr) ) {
+			lines[get_addr(s)] = instr;
 		}
-
-		// Address is located between beginning of line and first :
-		auto addr_begin = 0;
-		for ( ; isspace(s[addr_begin]); ++addr_begin);
-		const auto addr_len = s.find_first_of(':') - addr_begin;
-		const auto addr = s.substr(addr_begin, addr_len);
-
-		// Instructions begin after second tab; blank lines have only one
-		const auto first_tab = s.find_first_of('\t');
-		const auto second_tab = s.find_first_of('\t', first_tab+1);
-		if ( second_tab == string::npos ) {
-			continue;
-		}
-		const auto instr_begin = second_tab + 1;
-
-		// Instruction are terminated by eol, # or <
-		auto comment = s.find_last_of('#');
-		comment = comment == string::npos ? s.length() : comment;
-		auto annot = s.find_last_of('<');
-		annot = annot == string::npos ? s.length() : annot;
-		const auto instr_end = min(comment, annot);
-		const auto instr_len = (instr_end == string::npos ? s.length() : instr_end) - instr_begin;
-		const auto instr = s.substr(instr_begin, instr_len);
-
-		// Insert this line
-		lines[addr] = instr;
 	}
 
 	return lines;
@@ -154,6 +163,99 @@ label_map replace_label_uses(line_map& lines) {
 	return labels;
 }
 
+string fix_line(const string& line) {
+	// Replace retq synonyms
+	constexpr array<const char*, 2> rets {{"hlt    ","repz retq "}};
+	if ( find(rets.begin(), rets.end(), line) != rets.end() ) {
+		return "retq";
+	} 
+
+	// Replace nop synonyms
+	constexpr array<const char*, 3> nops {{"nopl","nopw","data"}};
+	if ( line.length() >= 4 && find(nops.begin(), nops.end(), line.substr(0,4)) != nops.end() ) {
+		return "nop";
+	}
+
+	// Add implicit trailing ones
+	constexpr array<const char*, 8> rots {{"shl","shr","sal","sar","rcl","rcr","rol","ror"}};
+	if ( line.length() >= 3 ) {
+		const auto in_list = find(rots.begin(), rots.end(), line.substr(0,3)) != rots.end();
+		const auto missing = line.find_first_of(',') == string::npos;
+		if ( in_list && missing) {
+			const auto split = line.find_first_of(' ');
+			return line.substr(0, split) + "$0x1," + line.substr(split+1);
+		} 
+	}
+
+	// Remove convenience naming for vector comparison instructions
+	constexpr array<const char*, 24> cmp7 {{
+		"cmpeqsd","cmpsd $0x0,","cmpeqss","cmpss $0x0,","vcmpeqsd","vcmpsd $0x0,", "vcmpeqss","vcmpss $0x0,",
+		"cmpltsd","cmpsd $0x1,","cmpltss","cmpss $0x1,","vcmpltsd","vcmpsd $0x1,", "vcmpltss","vcmpss $0x1,",
+		"cmplesd","cmpsd $0x2,","cmpless","cmpss $0x2,","vcmplesd","vcmpsd $0x2,", "vcmpless","vcmpss $0x2,"
+	}};
+	constexpr array<const char*, 8> cmp10 {{
+		"cmpunordsd","cmpsd $0x3,","cmpunordss","cmpss $0x3,","vcmpunordsd","vcmpsd $0x3,", "vcmpunordss","vcmpss $0x3,"
+	}};
+	constexpr array<const char*, 32> cmp8 {{
+		"cmpneqsd","cmpsd $0x4,","cmpneqss","cmpss $0x4,","vcmpneqsd","vcmpsd $0x4,", "vcmpneqss","vcmpss $0x4,",
+		"cmpnltsd","cmpsd $0x5,","cmpnltss","cmpss $0x5,","vcmpnltsd","vcmpsd $0x5,", "vcmpnltss","vcmpss $0x5,",
+		"cmpnlesd","cmpsd $0x6,","cmpnless","cmpss $0x6,","vcmpnlesd","vcmpsd $0x6,", "vcmpnless","vcmpss $0x6,",
+		"cmpordsd","cmpsd $0x7,","cmpordss","cmpss $0x7,","vcmpordsd","vcmpsd $0x7,", "vcmpordss","vcmpss $0x7,"
+	}};
+	if ( line.length() >= 7 ) {
+		auto itr = find(cmp7.begin(), cmp7.end(), line.substr(0,7));
+		if ( itr != cmp7.end() ) {
+			return *(++itr) + line.substr(8);
+		}
+	} 
+	if ( line.length() >= 10 ) {
+		auto itr = find(cmp10.begin(), cmp10.end(), line.substr(0,10));
+		if ( itr != cmp10.end() ) {
+			return *(++itr) + line.substr(11);
+		}
+	} 
+	if ( line.length() >= 8 ) {
+		auto itr = find(cmp8.begin(), cmp8.end(), line.substr(0,8));
+		if ( itr != cmp8.end() ) {
+			return *(++itr) + line.substr(9);
+		}
+	}
+
+	// Rename movabs for register arguments
+	if ( line.length() >= 9 && line.substr(0,9) == "movabsq $" ) {
+		return "movq " + line.substr(8);
+	}
+
+	// Add missing suffix to call and jmp
+	if ( line.length() >= 5 && line.substr(0,5) == "call " ) {
+		return "callq " + line.substr(5);
+	} 
+	if ( line.length() >= 4 && line.substr(0,4) == "jmp " ) {
+		return "jmpq " + line.substr(4);
+	}
+
+	// Remove documentation arg from string instructions
+	if ( line.length() >= 4 && line.substr(0,4) == "stos" ) {
+		const auto comma = line.find_first_of(',');
+		return line.substr(0,6) + line.substr(comma+1);
+	} 
+	if ( line.length() >= 8 && line.substr(0,8) == "rep stos" ) {
+		const auto comma = line.find_first_of(',');
+		return line.substr(0,10) + line.substr(comma+1);
+	} 
+	if ( line.length() >= 10 && line.substr(0,10) == "repnz scas" ) {
+		const auto comma = line.find_first_of(',');
+		return line.substr(0,comma);
+	}
+
+	// Make lock its own instruction
+	if ( line.length() >= 4 && line.substr(0,4) == "lock" ) {
+		return "lock\n" + line.substr(5);
+	}
+
+	return line;
+}
+
 void emit_fxn(const string& fxn, const line_map& lines, const label_map& labels) {
 	ofstream ofs(out.value() + "/" + fxn + ".s");
 	ofilterstream<Indent> os(ofs);
@@ -174,7 +276,7 @@ void emit_fxn(const string& fxn, const line_map& lines, const label_map& labels)
 			os << ".LABEL_" << itr->second << ":" << endl;
 			os.filter().indent();
 		}
-		os << l.second << endl;
+		os << fix_line(l.second) << endl;
 	}
 
 	os << ".size " << fxn << ", .-" << fxn << endl;
@@ -197,13 +299,13 @@ int main(int argc, char** argv) {
 	ifstream ifs(string("/tmp/stoke.") + getenv("USER") + ".objdump");
 	string s;
 
-	strip_header(ifs);
+	strip_prolog(ifs);
 	while ( getline(ifs, s) ) {
-		if ( ignore_section_header(ifs, s) ) {
+		if ( ignore_header(ifs, s) ) {
 			continue;
 		}
 	
-		const auto fxn = fxn_name(ifs, s);
+		const auto fxn = get_name(ifs, s);
 		auto lines = index_lines(ifs, s);
 		const auto labels = replace_label_uses(lines);
 		emit_fxn(fxn, lines, labels);
