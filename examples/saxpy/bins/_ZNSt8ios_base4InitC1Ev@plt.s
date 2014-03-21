b3d7406f8885047e98079496fb5cc8ce9af38947
eric <eric.schkufza@gmail.com>
2014-03-20 18:31:59 -0700

    These shouldn't have been checked in.

--- examples/saxpy/bins/_ZNSt8ios_base4InitC1Ev@plt.s
+++
@@ -1,8 +0,0 @@
-  .text
-  .globl _ZNSt8ios_base4InitC1Ev@plt
-  .type _ZNSt8ios_base4InitC1Ev@plt, @function
-_ZNSt8ios_base4InitC1Ev@plt:
-  jmpq   *0x200a7a(%rip)        
-  pushq  $0x1
-  jmpq   .L_400580
-  .size _ZNSt8ios_base4InitC1Ev@plt, .-_ZNSt8ios_base4InitC1Ev@plt
--- examples/saxpy/bins/__cxa_atexit@plt.s
+++
@@ -1,8 +0,0 @@
-  .text
-  .globl __cxa_atexit@plt
-  .type __cxa_atexit@plt, @function
-__cxa_atexit@plt:
-  jmpq   *0x200a6a(%rip)        
-  pushq  $0x3
-  jmpq   .L_400580
-  .size __cxa_atexit@plt, .-__cxa_atexit@plt
