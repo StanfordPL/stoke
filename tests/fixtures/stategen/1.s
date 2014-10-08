7f05226712ac32f97fe93266044b99b264787b55
Berkeley <berkeley@mrwhite>
2014-10-08 08:32:44 -0700

    Added a new fixture system and 8 testcases for stategen.

---
+++ tests/fixtures/stategen/1.s
@@ -0,0 +1,7 @@
+movl (%rax), %esi
+movq %rbp, %rcx
+xorl %eax, %eax
+movl $0x406a95, %edx
+xorl %edi, %edi
+xorl %ebp, %ebp
+retq
---
+++ tests/fixtures/stategen/4.s
@@ -0,0 +1,7 @@
+movl (%rax), %esi
+movq %rbp, %rcx
+xorl %edi, %edi
+xorl %eax, %eax
+movl $0x406a95, %edx
+xorl %ebp, %ebp
+retq
