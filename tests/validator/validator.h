77fdb1921b2cd02aebaa8c1a003647dbc293a8ed
Berkeley <berkeley@mrwhite>
2014-10-12 19:55:08 -0700

    Two new tests for the validator.

--- tests/validator/validator.h
+++ tests/validator/validator.h
@@ -83,6 +83,74 @@ TEST(Validator, UnimplementedFailsGracefully) {
   ASSERT_THROW(v.validate(cfg_t, cfg_r, tcs, ceg), validator_error);
 }
 
+
+TEST(Validator, SimpleCounterexample) {
+
+  x64asm::Code c, d;
+
+  std::stringstream tmp;
+  tmp << "movq $0x0, %rax" << std::endl;
+  tmp << "movq $0xc0decafe, %rdx" << std::endl;
+  tmp << "cmpq %rdx, %rcx" << std::endl;
+  tmp << "sete %al" << std::endl;
+  tmp << "movq $0x0, %rdx" << std::endl;
+  tmp << "retq" << std::endl;
+  tmp >> c;
+  tmp.str("");
+
+  tmp << "movq $0x0, %rax" << std::endl;
+  tmp << "movq $0x0, %rdx" << std::endl;
+  tmp << "retq" << std::endl;
+  tmp >> d;
+
+  stoke::Validator v(false);
+  stoke::CpuState tc;
+  stoke::CpuState ceg;
+
+  stoke::Cfg cfg_t(c, x64asm::RegSet::universe(), x64asm::RegSet::universe());
+  stoke::Cfg cfg_r(d, x64asm::RegSet::universe(), x64asm::RegSet::universe());
+
+  std::vector<stoke::CpuState> tcs;
+  tcs.push_back(tc);
+
+  EXPECT_FALSE(v.validate(cfg_t, cfg_r, tcs, ceg));
+
+  EXPECT_EQ(0xc0decafe, 0xffffffff & ceg.gp[1].get_fixed_quad(0));
+
+}
+
+
+TEST(Validator, ChecksUpper32bits) {
+
+  x64asm::Code c, d;
+
+  std::stringstream tmp;
+  tmp << "movq $0x1, %rax" << std::endl;
+  tmp << "shlq $0x28, %rax" << std::endl;
+  tmp >> c;
+  tmp.str("");
+
+  tmp << "movq $0x0, %rax" << std::endl;
+  tmp << "retq" << std::endl;
+  tmp >> d;
+
+  stoke::Validator v(false);
+  stoke::CpuState tc;
+  stoke::CpuState ceg;
+
+  stoke::Cfg cfg_t(c, x64asm::RegSet::universe(), x64asm::RegSet::universe());
+  stoke::Cfg cfg_r(d, x64asm::RegSet::universe(), x64asm::RegSet::universe());
+
+  std::vector<stoke::CpuState> tcs;
+  tcs.push_back(tc);
+
+  EXPECT_FALSE(v.validate(cfg_t, cfg_r, tcs, ceg));
+
+}
+
+
+
+
 TEST(Validator, DISABLED_ZeroTestcasesFailsGracefully) {
 
   x64asm::Code c;
