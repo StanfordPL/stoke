2216401f9326b142eb294b6cc111923180575bc6
Stefan Heule <stefanheule@gmail.com>
2014-11-07 15:17:42 -0800

    update copyright

--- src/args/opc_set.h
+++ src/args/opc_set.h
@@ -1,4 +1,4 @@
-// Copyright 2014 eric schkufza
+// Copyright 2014 eric schkufza, stefan heule
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
