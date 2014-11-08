23909b5c1d6b7905fb0c4239f2317920c2193891
Stefan Heule <stefanheule@gmail.com>
2014-11-08 13:00:55 -0800

    Automate more in zsh completion auto-gen, and modularize code.  Fixes #187.

--- tools/zsh_completion_generator.py
+++ tools/zsh_completion_generator.py
@@ -20,60 +20,257 @@ import subprocess
 import os
 import shutil
 
+
+# ------------------------------------------
+# main entry point
+# ------------------------------------------
+
+def main():
+  # build an AST with all the commands / arguments
+  cmd = get_command()
+
+  # show AST
+  # print cmd
+
+  handlers = [ZshHandler()]
+
+  # let each handler output it's file
+  for handler in handlers:
+    handler.handle(cmd)
+    handler.finish()
+
+  return
+
+
+
+# ------------------------------------------
+# handler class
+# ------------------------------------------
+
+# abstract handler that writes a completion script to a file
+# all actual handling of writing out the script is left abstract
+class Handler(object):
+  def __init__(self, filename):
+    self.filename = filename
+    self.full_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), ".." , "bin", filename)
+    self.file = open(self.full_filename, 'w')
+
+  def handle(self, command):
+    pass # to be implemented
+
+  def writeln(self, line):
+    print >>self.file, line
+
+  def finish(self):
+    self.file.close()
+
+
+
+# ------------------------------------------
+# a handler for zsh completion
+# ------------------------------------------
+
+class ZshHandler(Handler):
+  def __init__(self):
+    super(ZshHandler, self).__init__("_stoke")
+
+  def handle(self, command):
+    def aux(command):
+      if command.has_subcommands():
+        self.write_cmd(command)
+        for sub in command.subcommands:
+          aux(sub)
+      else:
+        self.write_arguments(command.name, command.arguments)
+    self.write_header()
+    aux(command)
+
+  def write_cmd(self, command):
+    assert len(command.arguments) == 0
+    assert len(command.subcommands) > 0
+    subhelp = []
+    subrun = []
+    for sub in command.subcommands:
+      subhelp.append("'" + sub.name + "':'" + sub.help + "'")
+      r = "(" + sub.name + ")"
+      r += "\n          " + "_" + sub.full_name().replace(" ", "_")
+      r += "\n        ;;"
+      subrun.append(r)
+    self.writeln("_" + command.full_name().replace(" ", "_") + """ ()
+{
+  local curcontext="$curcontext" state line
+  typeset -A opt_args
+
+  _arguments -C \\
+    ':command:->command' \\
+    '*::options:->options'
+
+  case $state in
+    (command)
+
+      local -a subcommands
+      subcommands=(
+        """ + "\n        ".join(subhelp) + """
+      )
+      _describe -t commands 'stoke' subcommands
+    ;;
+
+    (options)
+      case $line[1] in
+
+        """ + "\n\n        ".join(subrun) + """
+
+      esac
+    ;;
+  esac
+}""")
+
+  def write_arguments(self, name, args):
+    def esc(s):
+      return s.replace("{", "\\{").replace("}", "\\}").replace("'", "")
+    self.writeln("_stoke_" + name.replace(" ", "_") + "()\n{")
+    res = []
+    for arg in args:
+      if len(arg.get_parameters()) > 1:
+        a = "{'" + "','".join(arg.get_parameters()) + "'}'"
+      else:
+        a = "'" + arg.get_parameters()[0]
+
+      usage = arg.get_usage()
+
+      # no argument, just a flag
+      if usage == "":
+        res.append(a + "[" + esc(arg.desc) + "]::'")
+        continue
+
+      # number argument
+      m = re.search('<(int|double|percent|line|value|bytes)>$', usage)
+      if m != None:
+        res.append(a + "[" + esc(arg.desc + "; <"+m.group(1)+">") + "]:num:'")
+        continue
+
+      # file argument
+      m = re.search('<(path/to/(file|bin)\.?(.*))>$', usage)
+      if m != None:
+        if len(m.group(3)) > 0:
+          res.append(a + "[" + esc(arg.desc + "; <"+m.group(2)+">")+"]:file:_files -g \"*."+m.group(3)+"\"'")
+        else:
+          res.append(a + "[" + esc(arg.desc + "; <"+m.group(2)+">")+"]:file:_files'")
+        continue
+
+      # dir argument
+      m = re.search('<(path/to/dir|dir)>$', usage)
+      if m != None:
+        res.append(a + "[" + esc(arg.desc + "; <path/to/dir>")+"]:dir:_files -/'")
+        continue
+
+      # list of options
+      m = re.search('\((.*)\)$', usage)
+      if m != None:
+        options = m.group(1).split("|")
+        res.append(a + "[" + esc(arg.desc) + "]:argument:(" + " ".join(options) + ")'")
+        continue
+
+      # arguments without completion
+      m = re.search('({.*}|<move_type>|<arg1 arg2 ... argn>|<string>)$', usage)
+      if m != None:
+        options = m.group(1).split("|")
+        res.append(a + "[" + esc(arg.desc + "; "+m.group(1))+"]:argument:'")
+        continue
+
+      # no match, just add default help
+      print >> sys.stderr, "WARNING: Argument with unknown usage found: " + arg.name
+      res.append(a + "[" + esc(arg.desc) + "]:argument:'")
+
+    self.writeln("  _arguments \\")
+    self.writeln("    " + " \\\n    ".join(res))
+    self.writeln("}\n")
+
+  def write_header(self):
+    self.writeln("""#compdef stoke
+# ------------------------------------------------------------------------------
+#
+# Completion script for stoke.
+#
+# NOTE: Automatically generated, do not modify!
+#
+# Author: Stefan Heule <sheule@cs.stanford.edu>
+#
+# ------------------------------------------------------------------------------
+""")
+
+  def finish(self):
+    super(ZshHandler, self).finish()
+    # if ZSH_COMPLETION_DIR is set, copy file there
+    if os.environ.has_key('ZSH_COMPLETION_DIR'):
+      d = os.environ.get('ZSH_COMPLETION_DIR')
+      if os.path.isdir(d):
+        shutil.copy2(self.full_filename, os.path.join(d, self.filename))
+
+
+
+# ------------------------------------------
+# AST classes
+# ------------------------------------------
+
+# one argument to a given command
 class Arg(object):
   def __init__(self, arg, desc=""):
     self.name = arg
     self.desc = desc
+
+  # extend the description as necessary
   def add_desc(self, desc):
-    self.desc += desc
+    self.desc += " " + desc.strip()
     self.desc = self.desc.strip()
-  def args(self):
+
+  # get the list of parameter names (one or two)
+  def get_parameters(self):
     m = re.search("(-[^ ]*)( (-[^ ]*))?", self.name)
     if m.group(3) != None:
       return [m.group(1), m.group(3)]
     return [m.group(1)]
-  def extra(self):
-    return self.name[len(" ".join(self.args())):]
+
+  # return the usage part of the parameter
+  def get_usage(self):
+    return self.name[len(" ".join(self.get_parameters())):]
+
+  # simple string representation
   def __repr__(self):
     return self.name
 
-def main():
-  # generate stoke zsh completion file
-  filename = '_stoke'
-  localfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), ".." , "bin", filename)
-  f = open(localfile, 'w')
-  output_main(f)
-  output_function("extract", f)
-  output_function("replace", f)
-  output_function("testcase", f)
-  output_function("search", f)
-  output_function("debug cfg", f)
-  output_function("debug cost", f)
-  output_function("debug sandbox", f)
-  output_function("debug search", f)
-  output_function("debug state", f)
-  output_function("debug verify", f)
-  output_function("benchmark cfg", f)
-  output_function("benchmark cost", f)
-  output_function("benchmark sandbox", f)
-  output_function("benchmark search", f)
-  output_function("benchmark state", f)
-  output_function("benchmark verify", f)
-  f.close()
+# a stoke command (which has either a list of subcommands, or a list of arguments)
+class Command(object):
+  def __init__(self, parent, name, help, subcommands, arguments):
+    self.parent = parent
+    self.name = name.strip()
+    self.subcommands = subcommands
+    self.help = help.strip()
+    self.arguments = arguments
 
-  # if ZSH_COMPLETION_DIR is set, copy file there
-  if os.environ.has_key('ZSH_COMPLETION_DIR'):
-    d = os.environ.get('ZSH_COMPLETION_DIR')
-    if os.path.isdir(d):
-      shutil.copy2(localfile, os.path.join(d, filename))
+  def has_subcommands(self):
+    return len(self.subcommands) > 0
 
-def output_function(f, fid):
-  print >>fid, "__stoke_" + f.replace(" ", "_") + "()\n{"
-  process(get_output(f), fid)
-  print >>fid, "}\n"
+  def full_name(self):
+    if self.parent == None:
+      return self.name
+    return self.parent.full_name() + " " + self.name
 
-def get_output(cmd):
-  return run("stoke " + cmd + " --help | grep \"^  \(-\| \)\"")
+  def __repr__(self):
+    def indent(s):
+      return "  " + s.replace("\n", "\n  ")
+    s = self.name + " - " + self.help + "\n"
+    for sub in self.subcommands:
+      s += indent(str(sub)) + "\n"
+    for arg in self.arguments:
+      s += indent(str(arg)) + "\n"
+    return s
+
+
+
+# ------------------------------------------
+# parsing --help output into an AST
+# ------------------------------------------
 
 def run(cmd):
   p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -82,251 +279,48 @@ def run(cmd):
     out += line
   return out
 
-def esc(s):
-  return s.replace("{", "\\{").replace("}", "\\}").replace("'", "")
+def get_command():
+  output = run("stoke --help | grep \"^  \"")
+  subcommands = []
+  cmd = Command(None, "stoke", "", subcommands, [])
+  subsubmap = {}
+  for line in output.split("\n"):
+    if len(line) > 0:
+      match = re.search(r"^  (.*[^ ])  +([^ ].*)$", line)
+      help = match.group(2)
+      full = match.group(1)
+      parts = full.split(" ")
+      if (len(parts) == 1):
+        args = get_arguments(full)
+        sub = Command(cmd, full, help, [], args)
+        subcommands.append(sub)
+      else:
+        assert len(parts) == 2
+        if not parts[0] in subsubmap:
+          subsubmap[parts[0]] = []
+        subsubmap[parts[0]].append({
+          'name': parts[1],
+          'help': help
+        })
+  for sub, subsubs in subsubmap.iteritems():
+    subsubcommands = []
+    subcmd = Command(cmd, sub, sub + " commands", subsubcommands, [])
+    subcommands.append(subcmd)
+    for subsub in subsubs:
+      args = get_arguments(sub + " " + subsub['name'])
+      subsubcommands.append(Command(subcmd, subsub['name'], subsub['help'], [], args))
+  return cmd
 
-def process(data, fid):
+def get_arguments(subcommand):
+  data = run("stoke " + subcommand + " --help | grep \"^  \(-\| \)\"")
   args = []
   for line in data.split("\n"):
     if line.startswith("    "):
-      arg.add_desc(line[4:].strip())
+      arg.add_desc(line[4:])
     elif len(line) > 3:
       arg = Arg(line.strip())
       args.append(arg)
-  res = []
-  for arg in args:
-    if len(arg.args()) > 1:
-      a = "{'" + "','".join(arg.args()) + "'}'"
-    else:
-      a = "'" + arg.args()[0]
-
-    extra = arg.extra()
-
-    # no argument, just a flag
-    if extra == "":
-      res.append(a + "[" + esc(arg.desc) + "]::'")
-      continue
-
-    # number argument
-    m = re.search('<(int|double|percent|line|value|bytes)>$', extra)
-    if m != None:
-      res.append(a + "[" + esc(arg.desc + "; <"+m.group(1)+">") + "]:num:'")
-      continue
-
-    # file argument
-    m = re.search('<(path/to/(file|bin)\.?(.*))>$', extra)
-    if m != None:
-      if len(m.group(3)) > 0:
-        res.append(a + "[" + esc(arg.desc + "; <"+m.group(2)+">")+"]:file:_files -g \"*."+m.group(3)+"\"'")
-      else:
-        res.append(a + "[" + esc(arg.desc + "; <"+m.group(2)+">")+"]:file:_files'")
-      continue
-
-    # dir argument
-    m = re.search('<(path/to/dir|dir)>$', extra)
-    if m != None:
-      res.append(a + "[" + esc(arg.desc + "; <path/to/dir>")+"]:dir:_files -/'")
-      continue
-
-    # list of options
-    m = re.search('\((.*)\)$', extra)
-    if m != None:
-      options = m.group(1).split("|")
-      res.append(a + "[" + esc(arg.desc) + "]:argument:(" + " ".join(options) + ")'")
-      continue
-
-    # arguments without completion
-    m = re.search('({.*}|<move_type>|<arg1 arg2 ... argn>|<string>)$', extra)
-    if m != None:
-      options = m.group(1).split("|")
-      res.append(a + "[" + esc(arg.desc + "; "+m.group(1))+"]:argument:'")
-      continue
-
-    print "not found: " + arg.name
-    sys.exit(1)
-
-  print >>fid, "  _arguments \\"
-  print >>fid, "    " + " \\\n    ".join(res)
-
-def output_main(fid):
-  print >>fid, """#compdef stoke
-# ------------------------------------------------------------------------------
-#
-# Completion script for stoke.
-#
-# Author: Stefan Heule <sheule@cs.stanford.edu>
-#
-# ------------------------------------------------------------------------------
-
-_stoke ()
-{
-  local curcontext="$curcontext" state line
-  typeset -A opt_args
-
-  _arguments -C \\
-    ':command:->command' \\
-    '*::options:->options'
-
-  case $state in
-    (command)
-
-      local -a subcommands
-      subcommands=(
-        'extract':'extract the contents of a binary file'
-        'replace':'replace the contents of a binary file'
-        'search':'run STOKE search'
-        'testcase':'generate a STOKE testcase file'
-        'debug':'collection to help understand and debug STOKE'
-        'benchmark':'benchmark different aspects of STOKE'
-      )
-      _describe -t commands 'git flow' subcommands
-    ;;
-
-    (options)
-      case $line[1] in
-
-        (extract)
-          __stoke_extract
-        ;;
-
-        (replace)
-          __stoke_replace
-        ;;
-
-        (testcase)
-          __stoke_testcase
-        ;;
-
-        (search)
-          __stoke_search
-        ;;
-
-        (debug)
-          __stoke_debug
-        ;;
-
-        (benchmark)
-          __stoke_benchmark
-        ;;
-
-      esac
-    ;;
-  esac
-}
-
-__stoke_benchmark ()
-{
-  local curcontext="$curcontext" state line
-  typeset -A opt_args
-
-  _arguments -C \\
-    ':command:->command' \\
-    '*::options:->options'
-
-  case $state in
-    (command)
-
-      local -a subcommands
-      subcommands=(
-        'cfg':'benchmark Cfg::recompute() kernel'
-        'cost':'benchmark Cost::operator() kernel'
-        'sandbox':'benchmark Sandbox::run() kernel'
-        'search':'benchmark Transforms::modify() kernel'
-        'state':'benchmark Memory::copy_defined() kernel'
-        'verify':'benchmark Verifier::verify() kernel'
-      )
-      _describe -t commands 'stoke debug' subcommands
-    ;;
-
-    (options)
-      case $line[1] in
-
-        (cfg)
-          __stoke_benchmark_cfg
-        ;;
-
-        (cost)
-          __stoke_benchmark_cost
-        ;;
-
-        (sandbox)
-          __stoke_benchmark_sandbox
-        ;;
-
-        (search)
-          __stoke_benchmark_search
-        ;;
-
-        (state)
-          __stoke_benchmark_state
-        ;;
-
-        (verify)
-          __stoke_benchmark_verify
-        ;;
-
-      esac
-    ;;
-  esac
-}
-
-__stoke_debug ()
-{
-  local curcontext="$curcontext" state line
-  typeset -A opt_args
-
-  _arguments -C \\
-    ':command:->command' \\
-    '*::options:->options'
-
-  case $state in
-    (command)
-
-      local -a subcommands
-      subcommands=(
-        'cfg':'generate the control flow graph for a function'
-        'cost':'evaluate a function using a STOKE cost function'
-        'sandbox':'step through a function execution'
-        'search':'perform a program transformation'
-        'state':'check bit-wise operations'
-        'verify':'check the equivalence of two functions'
-      )
-      _describe -t commands 'stoke debug' subcommands
-    ;;
-
-    (options)
-      case $line[1] in
-
-        (cfg)
-          __stoke_debug_cfg
-        ;;
-
-        (cost)
-          __stoke_debug_cost
-        ;;
-
-        (sandbox)
-          __stoke_debug_sandbox
-        ;;
-
-        (search)
-          __stoke_debug_search
-        ;;
-
-        (state)
-          __stoke_debug_state
-        ;;
-
-        (verify)
-          __stoke_debug_verify
-        ;;
-
-      esac
-    ;;
-  esac
-}
-"""
-
+  return args
 
 if __name__ == '__main__':
   main()
