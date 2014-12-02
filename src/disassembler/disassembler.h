bd2c60577d814a4632ae7d9130911db97da42b2b
eric <eric.schkufza@gmail.com>
2014-12-02 14:36:44 -0800

    Updated copyright notice everywhere.

--- src/disassembler/disassembler.h
+++ src/disassembler/disassembler.h
@@ -1,4 +1,4 @@
-// Copyright 2014 berkeley churchill
+// Copyright 2013-2015 Eric Schkufza, Rahul Sharma, Berkeley Churchill, Stefan Heule
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
