d976ef98da1364b2af1c0274605779dfe0c2c9ed
Berkeley Churchill (on mrwhite) <berkeley@berkeleychurchill.com>
2014-12-02 17:27:42 -0800

    Isolated sbb issue for #238

---
+++ tests/validator/handlers/sbb.h
@@ -0,0 +1,51 @@
+// Copyright 2013-2015 Eric Schkufza, Rahul Sharma, Berkeley Churchill, Stefan Heule
+//
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an AS IS BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+class ValidatorSbbTest : public ValidatorTest {};
+
+TEST_F(ValidatorSbbTest, Identity) {
+
+  target_ << "sbbq $0x10, %rax" << std::endl;
+  target_ << "retq" << std::endl;
+
+  rewrite_ << "sbbq $0x10, %rax" << std::endl;
+  rewrite_ << "retq" << std::endl;
+
+  assert_equiv();
+
+}
+
+TEST_F(ValidatorSbbTest, NotANoop) {
+
+  target_ << "sbbq $0x10, %rax" << std::endl;
+  target_ << "retq" << std::endl;
+
+  rewrite_ << "retq" << std::endl;
+
+  assert_ceg();
+
+}
+
+
+TEST_F(ValidatorSbbTest, Issue238) {
+
+  target_ << "sbbw $0xfffb, %ax" << std::endl;
+  target_ << "retq" << std::endl;
+
+  stoke::CpuState cs;
+  cs.gp[x64asm::ax].get_fixed_quad(0) = 0xb0a02d08;
+
+  check_circuit(cs);
+}
