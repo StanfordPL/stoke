818c050d0502eb0010f6514356189099d753a500
Berkeley Churchill (on mrwhite) <berkeley@berkeleychurchill.com>
2014-12-05 02:02:59 -0800

    alias analysis test runs (and fails, duh)

--- src/analysis/alias.h
+++ src/analysis/alias.h
@@ -2,28 +2,31 @@
 #ifndef STOKE_SRC_ANALYSIS_ALIASING_H
 #define STOKE_SRC_ANALYSIS_ALIASING_H
 
+#include "src/cfg/cfg.h"
+#include "src/ext/x64asm/include/x64asm.h"
+
 namespace stoke {
 
 class AliasAnalysis {
 
 public:
-  AliasAnalysis(const Cfg& cfg) : cfg_(cfg) {}
+  AliasAnalysis(const x64asm::Code& code) : code_(code) {}
 
   /** Tells if two memory references must overlap *exactly* */
   template <class T1, class T2>
-  bool must_overlap(size_t line_1, M<T1> ref_1, size_t line_2, M<T2> ref_2) {
+  bool must_overlap(size_t line_1, x64asm::M<T1> ref_1, size_t line_2, x64asm::M<T2> ref_2) {
     return false;
   }
 
   /** Tells if two memory references may overlap *at all* */
   template <class T1, class T2>
-  bool may_overlap(size_t line_1, M<T1> ref_1, size_t line_2, M<T2> ref_2) {
+  bool may_overlap(size_t line_1, x64asm::M<T1> ref_1, size_t line_2, x64asm::M<T2> ref_2) {
     return true;
   }
 
 private:
 
-  const Cfg& cfg_;
+  const x64asm::Code& code_;
 };
 
 
---
+++ tests/analysis/analysis.h
@@ -0,0 +1,2 @@
+
+#include "alias.h"
