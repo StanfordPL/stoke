cb04941427635372cb145b83676b943c28b7d393
Stefan Heule <stefanheule@gmail.com>
2014-12-05 01:23:03 -0800

    Improve and fix --init zero

--- tests/search/search.h
+++ tests/search/search.h
@@ -35,24 +35,31 @@ protected:
   x64asm::RegSet def_in_;
   x64asm::RegSet live_out_;
 
+  void test(std::string in, std::string out) {
+    std::stringstream ss0;
+    std::stringstream ss1;
+    ss0 << "{ " << in << " }";
+    ss0 >> live_out_;
+    ss1 << "{ " << out << " }";
+    ss1 >> def_in_;
+
+    auto code = stoke::Search::find_sound_code(def_in_, live_out_);
+    auto cfg = stoke::Cfg(code, def_in_, live_out_);
+    ASSERT_TRUE(cfg.is_sound()) << "def in:   " << def_in_ << std::endl <<
+                                "live out: " << live_out_ << std::endl <<
+                                "cfg.def_outs(): " << cfg.def_outs() << std::endl <<
+                                "code: " << code << std::endl;
+  }
+
 };
 
 TEST_P(SearchTest, ZeroInit) {
-  std::stringstream ss;
-  std::string s = "{ ";
-  s += GetParam();
-  s += " }";
-  ss << s;
-  ss >> live_out_;
-  ss << "{ }";
-  ss >> def_in_;
-  auto code = stoke::Search::find_sound_code(def_in_, live_out_);
-  auto cfg = stoke::Cfg(code, def_in_, live_out_);
-  ASSERT_TRUE(cfg.is_sound()) << "def in:   " << def_in_ << std::endl <<
-                              "live out: " << live_out_ << std::endl <<
-                              "cfg.def_outs(): " << cfg.def_outs();
+  test("", GetParam());
 }
 
+TEST_F(SearchTest, ZeroInitBug0) {
+  test("%ymm8", "%xmm8 %xmm9");
+}
 
 INSTANTIATE_TEST_CASE_P(GeneralPurpose0, SearchTest, ::testing::Values(
                           "%rax", "%eax", "%ax", "%al", "%rbx", "%ebx", "%bx", "%bl", "%rcx", "%ecx", "%cx", "%cl", "%rdx", "%edx", "%dx", "%dl", "%rsi", "%esi", "%si", "%sil", "%rdi", "%edi", "%di", "%dil", "%rbp", "%ebp", "%bp", "%bpl", "%rsp", "%esp", "%sp", "%spl"
@@ -62,9 +69,13 @@ INSTANTIATE_TEST_CASE_P(GeneralPurpose1, SearchTest, ::testing::Values(
                           "%r8", "%r8d", "%r8w", "%r8b", "%r9", "%r9d", "%r9w", "%r9b", "%r10", "%r10d", "%r10w", "%r10b", "%r11", "%r11d", "%r11w", "%r11b", "%r12", "%r12d", "%r12w", "%r12b", "%r13", "%r13d", "%r13w", "%r13b", "%r14", "%r14d", "%r14w", "%r14b", "%r15", "%r15d", "%r15w", "%r15b"
                         ));
 
-INSTANTIATE_TEST_CASE_P(Sse, SearchTest, ::testing::Values(
+INSTANTIATE_TEST_CASE_P(Sse0, SearchTest, ::testing::Values(
                           "%xmm0", "%xmm1", "%xmm2", "%xmm3", "%xmm4", "%xmm5","%xmm6", "%xmm7", "%xmm8", "%xmm9", "%xmm10", "%xmm11", "%xmm12", "%xmm13", "%xmm14", "%xmm15",
                           "%ymm0", "%ymm1", "%ymm2", "%ymm3", "%ymm4", "%ymm5","%ymm6", "%ymm7", "%ymm8", "%ymm9", "%ymm10", "%ymm11", "%ymm12", "%ymm13", "%ymm14", "%ymm15"
+                        ));
+
+INSTANTIATE_TEST_CASE_P(Sse1, SearchTest, ::testing::Values(
+                          "%mm0", "%mm1", "%mm2", "%mm3", "%mm4", "%mm5","%mm6", "%mm7"
                         ));
 
 INSTANTIATE_TEST_CASE_P(Flags, SearchTest, ::testing::Values(
