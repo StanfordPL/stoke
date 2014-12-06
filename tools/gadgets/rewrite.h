e5a16c6710e47a688668d92bd45248ac9693a77c
eric <eric.schkufza@gmail.com>
2014-12-05 20:58:14 -0800

    Checking rewrite for unsupported instructions (by way of sandbox and user hardware)

--- tools/gadgets/rewrite.h
+++ tools/gadgets/rewrite.h
@@ -15,15 +15,36 @@
 #ifndef STOKE_TOOLS_GADGETS_REWRITE_H
 #define STOKE_TOOLS_GADGETS_REWRITE_H
 
+#include "src/ext/x64asm/include/x64asm.h"
+
 #include "src/cfg/cfg.h"
+#include "src/sandbox/sandbox.h"
 #include "tools/args/rewrite.h"
 #include "tools/args/target.h"
+#include "tools/target/cpu_info.h"
+#include "tools/ui/console.h"
 
 namespace stoke {
 
 class RewriteGadget : public Cfg {
 public:
   RewriteGadget() : Cfg(rewrite_arg.value().code, def_in_arg, live_out_arg) {
+		// Check for instructions that require unavailable flags
+		const auto cpu_flags = CpuInfo::get_flags();
+		auto target_flags = get_code().required_flags();
+    if (!cpu_flags.contains(target_flags)) {
+      const auto diff = target_flags - cpu_flags;
+      Console::error(1) << "Target requires unavailable cpu flags: " << diff << std::endl;
+    }
+
+		// Check for instructions that aren't supported by the sandbox
+		for (const auto& instr : get_code()) {
+			if (!Sandbox::is_supported(instr)) {
+				Console::error(1) << "Target contains an unsupported instruction: " << instr << std::endl;
+			}
+		}
+
+		// Add summaries for auxiliary functions
     for (const auto& fxn : aux_fxns_arg.value()) {
       auto code = fxn.code;
       auto lbl = code[0].get_operand<x64asm::Label>(0);
