58dc0d34c3a5ee2d901d1021590fcdcbf5d0e8d9
eric <eric.schkufza@gmail.com>
2014-12-05 20:43:29 -0800

    Refactored stefan's cpu id reader class.

---
+++ tools/target/cpu_info.h
@@ -0,0 +1,15 @@
+#ifndef STOKE_TOOLS_TARGET_CPU_INFO_H
+#define STOKE_TOOLS_TARGET_CPU_INFO_H
+
+#include "src/ext/x64asm/include/x64asm.h"
+
+namespace stoke {
+
+class CpuInfo {
+	public:
+		static x64asm::FlagSet get_flags();
+};
+
+} // namespace stoke
+
+#endif
