ba61dc3781bdfd19220040b8fa910d9944ec3d1a
Berkeley Churchill (on mrwhite) <berkeley@berkeleychurchill.com>
2014-12-12 15:55:10 -0800

    3801->3803

--- src/validator/handler.cc
+++ src/validator/handler.cc
@@ -34,7 +34,7 @@ bool Handler::operands_supported(const Instruction& instr) {
 }
 
 
-const array<const char*, 3801> Handler::att_ = {{
+const array<const char*, 3803> Handler::att_ = {{
     "<label definition>"
 #include "src/ext/x64asm/src/opcode.att"
   }
