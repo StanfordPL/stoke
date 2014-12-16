228229072bac92bf37b657c48461f8895689db83
Berkeley Churchill (on mrwhite) <berkeley@berkeleychurchill.com>
2014-12-16 14:43:18 -0800

    Producing counterexamples for memory.

--- src/validator/validator.cc
+++ src/validator/validator.cc
@@ -12,29 +12,22 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <ctime>
-#include <fstream>
 #include <string>
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <map>
 #include <list>
-#include <set>
 #include <cassert>
-#include <tr1/unordered_set>
-#include <locale>
 #include <initializer_list>
-#include <unistd.h>
 
+#include "src/state/cpu_state.h"
 #include "src/symstate/bitvector.h"
 #include "src/validator/validator.h"
 
 using namespace std;
 using namespace stoke;
 using namespace x64asm;
-
-
 
 
 bool regset_is_supported(x64asm::RegSet rs) {
@@ -81,7 +74,7 @@ bool Validator::is_supported(Instruction& i) {
 }
 
 
-void Validator::generate_constraints(const stoke::Cfg& f1, const stoke::Cfg& f2, 
+void Validator::generate_constraints(const stoke::Cfg& f1, const stoke::Cfg& f2,
                                      SymState& f1_final, SymState& f2_final,
                                      vector<SymBool>& constraints) const {
 
@@ -194,6 +187,164 @@ void Validator::build_circuit(const Cfg& cfg, SymState& state) const {
   }
 }
 
+/** Takes a *sorted* list of addresses along with the size of the read/write.
+ * Determines the stack/heap bounds if we put the first 'pos' addresses in the
+ * heap and the remaining 'count - pos' addresses in the stack.  Also checks
+ * that the split is "valid", i.e. no read is both in the stack/heap; this
+ * check is needed because the sorting doesn't account for overlapping reads
+ * (this is the boolean returned). */
+bool compute_split(vector<pair<uint64_t, uint16_t>> addresses, size_t pos,
+                   uint64_t& heap_min, uint64_t& heap_size,
+                   uint64_t& stack_min, uint64_t& stack_size) {
+
+  size_t count = addresses.size();
+
+  // Compute heap bounds
+  if(pos) {
+    heap_min = addresses[0].first;
+    heap_size = addresses[pos-1].first + addresses[pos-1].second - heap_min;
+  } else {
+    heap_min = 0;
+    heap_size = 0;
+  }
+
+  // Compute stack bounds
+  if(pos == count) {
+    stack_min = 0;
+    stack_size = 0;
+  } else {
+    stack_min = addresses[pos].first;
+    stack_size = addresses[count-1].first + addresses[count-1].second - stack_min;
+  }
+
+  // Check the bounds work
+  for(auto p : addresses) {
+    if (p.first >= heap_min && (p.first + p.second) <= (heap_min + heap_size - 1))
+      continue;
+    if (p.first >= stack_min && (p.first + p.second) <= (stack_min + stack_size - 1))
+      continue;
+
+    return false;
+  }
+
+  return true;
+}
+
+CpuState Validator::state_from_model(SMTSolver& smt, const string& name_suffix,
+                                     const SymMemory* memory, const SymMemory* memory2) {
+
+  CpuState cs;
+
+  // Get the values of registers
+  for(size_t i = 0; i < r64s.size(); ++i) {
+    stringstream name;
+    name << r64s[i] << name_suffix;
+    cs.gp[r64s[i]] = smt.get_model_bv(name.str(), 1);
+  }
+
+  for(size_t i = 0; i < ymms.size(); ++i) {
+    stringstream name;
+    name << ymms[i] << name_suffix;
+    cs.sse[ymms[i]] = smt.get_model_bv(name.str(), 4);
+  }
+
+  for(size_t i = 0; i < eflags.size(); ++i) {
+    if(!cs.rf.is_status(eflags[i].index()))
+      continue;
+
+    stringstream name;
+    name << eflags[i] << name_suffix;
+    cs.rf.set(eflags[i].index(), smt.get_model_bool(name.str()));
+  }
+
+  // Figure out error code
+  if(smt.get_model_bool("sigbus" + name_suffix)) {
+    cs.code = ErrorCode::SIGBUS_;
+  } else if (smt.get_model_bool("sigfpe" + name_suffix)) {
+    cs.code = ErrorCode::SIGFPE_;
+  } else if (smt.get_model_bool("sigsegv" + name_suffix)) {
+    cs.code = ErrorCode::SIGSEGV_;
+  } else {
+    cs.code = ErrorCode::NORMAL;
+  }
+
+  // Figure out memory, if needed (this is the last thing).  We can get a list
+  // of concrete addresses accessed, and need to split these addresses into
+  // heap/stack.  My goal is to just optimize the size of each of the heap and
+  // the stack; the algorithm is to just sort the addresses and try every
+  // possible place to split them.  This is O(n^2) where n is the number of
+  // addresses.  The sorting O(n*log n) is dominated by the loop where we check
+  // the goodness of each split; calculating the goodness costs O(n) and we
+  // need to do this O(n) times.
+
+  if(!memory && !memory2) {
+    cs.stack.resize(0, 0);
+    cs.heap.resize(0, 0);
+    return cs;
+  }
+
+  auto concrete = vector<pair<uint64_t, uint16_t>>();
+  if(memory) {
+    for(auto p : memory->get_address_vars()) {
+      concrete.push_back(pair<uint64_t, uint16_t>(smt.get_model_bv(p.first, 1).get_fixed_quad(0), p.second));
+    }
+  }
+  if(memory2) {
+    for(auto p : memory2->get_address_vars()) {
+      concrete.push_back(pair<uint64_t, uint16_t>(smt.get_model_bv(p.first, 1).get_fixed_quad(0), p.second));
+    }
+  }
+
+  auto compare = [] (const pair<uint64_t, uint16_t>& p1, const pair<uint64_t, uint16_t>& p2) {
+    if(p1.first - p2.first == 0)
+      return (uint64_t)(p1.second - p2.second);
+    return p1.first - p2.first;
+  };
+  sort(concrete.begin(), concrete.end(), compare);
+
+  uint64_t heap_min, heap_size, stack_min, stack_size;
+  bool ok = compute_split(concrete, 0, heap_min, heap_size, stack_min, stack_size);
+  assert(ok);
+
+  uint64_t best = heap_size + stack_size;
+  size_t best_index = 0;
+  for (size_t i = 1; i < concrete.size(); ++i) {
+    ok = compute_split(concrete, i, heap_min, heap_size, stack_min, stack_size);
+    if(!ok)
+      continue;
+    uint64_t quality = heap_size + stack_size;
+    if(quality < best) {
+      best = quality;
+      best_index = i;
+    }
+  }
+
+  ok = compute_split(concrete, best_index, heap_min, heap_size, stack_min, stack_size);
+  assert(ok);
+
+  cs.stack.resize(stack_min, stack_size); //base, size
+  cs.heap.resize(heap_min, heap_size);  //base, size
+
+  // Now set the defined bits
+  for(auto p : concrete) {
+    if(cs.stack.in_range(p.first) && cs.stack.in_range(p.first + p.second - 1)) {
+      for(size_t i = 0; i < p.second; ++i) {
+        cs.stack.set_valid(p.first + i, true);
+      }
+    }
+    if(cs.heap.in_range(p.first) && cs.heap.in_range(p.first + p.second - 1)) {
+      for(size_t i = 0; i < p.second; ++i) {
+        cs.heap.set_valid(p.first + i, true);
+      }
+    }
+  }
+
+  return cs;
+
+}
+
+
+
 
 bool Validator::validate(const Cfg& target, const Cfg& rewrite, CpuState& counter_example)
 {
@@ -221,9 +372,9 @@ bool Validator::validate(const Cfg& target, const Cfg& rewrite, CpuState& counte
     if (is_sat && solver_.has_model()) {
 
       counterexample_valid_ = true;
-      counterexample_ =      CpuState(solver_, "_", &target_final.memory, &rewrite_final.memory);
-      target_final_state_  = CpuState(solver_, "_1_FINAL");
-      rewrite_final_state_ = CpuState(solver_, "_2_FINAL");
+      counterexample_ =      state_from_model(solver_, "_", &target_final.memory, &rewrite_final.memory);
+      target_final_state_  = state_from_model(solver_, "_1_FINAL");
+      rewrite_final_state_ = state_from_model(solver_, "_2_FINAL");
 
       counter_example = counterexample_;
 
@@ -244,4 +395,6 @@ bool Validator::validate(const Cfg& target, const Cfg& rewrite, CpuState& counte
     return 0;
   }
 }
+
+
 
