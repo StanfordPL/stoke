32db334f4edfba638064f79d6f0e6933f70bc8a1
Berkeley Churchill (on mrwhite) <berkeley@berkeleychurchill.com>
2014-12-16 13:47:42 -0800

    about to move state construction function to someplace else

--- src/state/cpu_state.cc
+++ src/state/cpu_state.cc
@@ -12,8 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <algorithm>
+
 #include "src/ext/x64asm/include/x64asm.h"
 #include "src/state/cpu_state.h"
+#include "src/symstate/memory.h"
 
 using namespace cpputil;
 using namespace std;
@@ -91,11 +94,64 @@ istream& CpuState::read_text(istream& is) {
   return is;
 }
 
-void CpuState::convert_from_model(SMTSolver& smt, string& name_suffix) {
+/* Swaps two (address, size) pairs */
+void swap(uint64_t& address1, uint16_t& size1, uint64_t& address2, uint16_t& size2) {
+  uint64_t tmpa = address1;
+  address1 = address2;
+  address2 = tmpa;
 
-  stack.resize(0, 0); //base, size
-  heap.resize(0, 0);  //base, size
+  uint64_t tmps = size1;
+  size1 = size2;
+  size2 = tmps;
+}
 
+/** Takes a *sorted* list of addresses along with the size of the read/write.
+ * Determines the stack/heap bounds if we put the first 'pos' addresses in the
+ * heap and the remaining 'count - pos' addresses in the stack.  Also checks
+ * that the split is "valid", i.e. no read is both in the stack/heap; this
+ * check is needed because the sorting doesn't account for overlapping reads
+ * (this is the boolean returned). */
+bool compute_split(vector<pair<uint64_t, uint16_t>> addresses, size_t pos,
+                   uint64_t& heap_min, uint64_t& heap_size, 
+                   uint64_t& stack_min, uint64_t& stack_size) {
+
+  size_t count = addresses.size();
+
+  // Compute heap bounds
+  if(pos) {
+    heap_min = addresses[0].first;
+    heap_size = addresses[pos-1].first + addresses[pos-1].second - heap_min;
+  } else {
+    heap_min = 0;
+    heap_size = 0;
+  }
+
+  // Compute stack bounds
+  if(pos == count) {
+    stack_min = 0;
+    stack_size = 0;
+  } else {
+    stack_min = addresses[pos].first;
+    stack_size = addresses[count-1].first + addresses[count-1].second - stack_min;
+  }
+
+  // Check the bounds work
+  for(auto p : addresses) {
+    if (p.first >= heap_min && (p.first + p.second) <= (heap_min + heap_size - 1))
+      continue;
+    if (p.first >= stack_min && (p.first + p.second) <= (stack_min + stack_size - 1))
+      continue;
+
+    return false;
+  }
+
+  return true;
+}
+
+void CpuState::convert_from_model(SMTSolver& smt, string& name_suffix, 
+                                  SymMemory* memory, SymMemory* memory2) {
+
+  // Get the values of registers
   for(size_t i = 0; i < r64s.size(); ++i) {
     stringstream name;
     name << r64s[i] << name_suffix;
@@ -117,6 +173,7 @@ void CpuState::convert_from_model(SMTSolver& smt, string& name_suffix) {
     rf.set(eflags[i].index(), smt.get_model_bool(name.str()));
   }
 
+  // Figure out error code
   if(smt.get_model_bool("sigbus" + name_suffix)) {
     code = ErrorCode::SIGBUS_;
   } else if (smt.get_model_bool("sigfpe" + name_suffix)) {
@@ -125,6 +182,81 @@ void CpuState::convert_from_model(SMTSolver& smt, string& name_suffix) {
     code = ErrorCode::SIGSEGV_;
   } else {
     code = ErrorCode::NORMAL;
+  }
+
+  // Figure out memory, if needed (this is the last thing).  We can get a list
+  // of concrete addresses accessed, and need to split these addresses into
+  // heap/stack.  My goal is to just optimize the size of each of the heap and
+  // the stack; the algorithm is to just sort the addresses and try every
+  // possible place to split them.  This is O(n^2) where n is the number of
+  // addresses.  The sorting O(n*log n) is dominated by the loop where we check
+  // the goodness of each split; calculating the goodness costs O(n) and we
+  // need to do this O(n) times.
+
+  if(!memory && !memory2)
+    return;
+
+  auto concrete = vector<pair<uint64_t, uint16_t>>();
+  if(memory) {
+    for(auto p : memory->get_address_vars()) {
+      concrete.push_back(pair<uint64_t, uint16_t>(smt.get_model_bv(p.first, 1).get_fixed_quad(0), p.second));
+    }
+  }
+  if(memory2) {
+    for(auto p : memory2->get_address_vars()) {
+      concrete.push_back(pair<uint64_t, uint16_t>(smt.get_model_bv(p.first, 1).get_fixed_quad(0), p.second));
+    }
+  }
+
+
+
+  auto compare = [] (const pair<uint64_t, uint16_t>& p1, const pair<uint64_t, uint16_t>& p2) {
+    if(p1.first - p2.first == 0)
+      return (uint64_t)(p1.second - p2.second);
+    return p1.first - p2.first;
+  };
+  sort(concrete.begin(), concrete.end(), compare);
+
+  uint64_t heap_min, heap_size, stack_min, stack_size;
+  bool ok = compute_split(concrete, 0, heap_min, heap_size, stack_min, stack_size);
+  assert(ok);
+
+  uint64_t best = heap_size + stack_size;
+  size_t best_index = 0;
+  for (size_t i = 1; i < concrete.size(); ++i) {
+    ok = compute_split(concrete, i, heap_min, heap_size, stack_min, stack_size);
+    if(!ok)
+      continue;
+    uint64_t quality = heap_size + stack_size;
+    if(quality < best) {
+      best = quality;
+      best_index = i;
+    }
+  }
+
+  ok = compute_split(concrete, best_index, heap_min, heap_size, stack_min, stack_size);
+  assert(ok);
+
+  stack.resize(stack_min, stack_size); //base, size
+  heap.resize(heap_min, heap_size);  //base, size
+  cout << "STACK_MIN= " << stack_min << " STACK_SIZE=" << stack_size << endl;
+  cout << "HEAP_MIN= " << heap_min << " HEAP_SIZE=" << heap_size << endl;
+
+  // Now set the defined bits
+  for(auto p : concrete) {
+    cout << "Checking out " << hex << p.first << " (size " << p.second << ")" << endl;
+    if(stack.in_range(p.first) && stack.in_range(p.first + p.second - 1)) {
+      cout << " - it's in the stack" << endl;
+      for(size_t i = 0; i < p.second; ++i) {
+        stack.set_defined(p.first + i, true);
+      }
+    }
+    if(heap.in_range(p.first) && heap.in_range(p.first + p.second - 1)) {
+      cout << " - it's in the heap" << endl;
+      for(size_t i = 0; i < p.second; ++i) {
+        heap.set_defined(p.first + i, true);
+      }
+    }
   }
 
 }
