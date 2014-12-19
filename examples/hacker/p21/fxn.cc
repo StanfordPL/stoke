92afe85dfd64a78bdca1c32042d917bc7ae31af7
eric <eric.schkufza@gmail.com>
2014-12-19 15:52:01 -0800

    Another one works.

--- examples/hacker/p21/fxn.cc
+++ examples/hacker/p21/fxn.cc
@@ -14,21 +14,13 @@
 
 #include <stdint.h>
 
-int32_t p25(int32_t x, int32_t y) {
-  uint32_t o1 = x & 0x0000ffff;
-  int32_t o2 = x >> 16;
-  uint32_t o3 = y & 0x0000ffff;
-  int32_t o4 = y >> 16;
-  uint32_t o5 = o1 * o3;
-  int32_t o6 = o2 * o3;
-  int32_t o7 = o1 * o4;
-  int32_t o8 = o2 * o4;
-  int32_t o9 = o5 >> 16;
-  int32_t o10 = o6 + o9;
-  int32_t o11 = o10 & 0x0000ffff;
-  int32_t o12 = o10 >> 16;
-  int32_t o13 = o7 + o11;
-  int32_t o14 = o13 >> 16;
-  int32_t o15 = o14 + o12;
-  return o15 + o8;
+int32_t p21(int32_t x, int32_t a, int32_t b, int32_t c) {
+	  int32_t o1 = -(x == c);
+		  int32_t o2 = a ^ c;
+			  int32_t o3 = -(x == a);
+				  int32_t o4 = b ^ c;
+					  int32_t o5 = o1 & o2;
+						  int32_t o6 = o3 & o4;
+							  int32_t o7 = o5 ^ o6;
+								  return o7 ^ c;
 }
