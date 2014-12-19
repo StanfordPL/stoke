4639586d1c3c351a9ee80f3d1be20e188eeb93af
eric <eric.schkufza@gmail.com>
2014-12-19 15:08:23 -0800

    Lots of new examples.

---
+++ examples/hacker/p02/fxn.cc
@@ -0,0 +1,20 @@
+// Copyright 2013-2015 Eric Schkufza, Rahul Sharma, Berkeley Churchill, Stefan Heule
+//
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an AS IS BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stdint.h>
+
+int32_t p02(int32_t x) {
+	int32_t o1 = x + 1;
+	return x & o1;
+}
