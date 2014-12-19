3fd192c881d00d6770ea149eb7469cdf4d31034d
eric <eric.schkufza@gmail.com>
2014-12-19 15:25:29 -0800

    More winners.

--- examples/hacker/p25/fxn.cc
+++ examples/hacker/p25/fxn.cc
@@ -14,13 +14,21 @@
 
 #include <stdint.h>
 
-int32_t p22(int32_t x) {
-  int32_t o1 = x >> 1;
-  int32_t o2 = o1 ^ x;
-  int32_t o3 = o2 >> 2;
-  int32_t o4 = o2 ^ o3;
-  int32_t o5 = o4 & 0x11111111;
-  int32_t o6 = o5 * 0x11111111;
-  int32_t o7 = o6 >> 28;
-  return o7 & 0x1;
+int32_t p25(int32_t x, int32_t y) {
+	  uint32_t o1 = x & 0x0000ffff;
+		  int32_t o2 = x >> 16;
+			  uint32_t o3 = y & 0x0000ffff;
+				  int32_t o4 = y >> 16;
+					  uint32_t o5 = o1 * o3;
+						  int32_t o6 = o2 * o3;
+							  int32_t o7 = o1 * o4;
+								  int32_t o8 = o2 * o4;
+									  int32_t o9 = o5 >> 16;
+										  int32_t o10 = o6 + o9;
+											  int32_t o11 = o10 & 0x0000ffff;
+												  int32_t o12 = o10 >> 16;
+													  int32_t o13 = o7 + o11;
+														  int32_t o14 = o13 >> 16;
+															  int32_t o15 = o14 + o12;
+																  return o15 + o8;
 }
