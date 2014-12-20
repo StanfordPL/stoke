54b859a5bff354a811cc35d851ed1e6e5124ef3b
eric <eric.schkufza@gmail.com>
2014-12-19 17:47:59 -0800

    astyle

--- examples/hacker/p14/fxn.cc
+++ examples/hacker/p14/fxn.cc
@@ -15,8 +15,8 @@
 #include <stdint.h>
 
 int32_t p14(int32_t x, int32_t y) {
-	  int32_t o1 = x & y;
-		  int32_t o2 = x ^ y;
-			  int32_t o3 = o2 >> 1;
-				  return o1 + o3;
+  int32_t o1 = x & y;
+  int32_t o2 = x ^ y;
+  int32_t o3 = o2 >> 1;
+  return o1 + o3;
 }
