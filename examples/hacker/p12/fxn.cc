0d40f02604c62e685106b30e9749dc04d4a4e4a9
eric <eric.schkufza@gmail.com>
2014-12-19 17:16:27 -0800

    Getting pretty close here.

--- examples/hacker/p12/fxn.cc
+++ examples/hacker/p12/fxn.cc
@@ -14,9 +14,8 @@
 
 #include <stdint.h>
 
-int32_t p16(int32_t x, int32_t y) {
-  int32_t o1 = x ^ y;
-  int32_t o2 = -((uint32_t) x >= (uint32_t) y);
-  int32_t o3 = o1 & o2;
-  return o3 ^ y;
+int32_t p12(int32_t x, int32_t y) {
+	  int32_t o1 = ~y;
+		  int32_t o2 = x & o1;
+			  return (uint32_t) o2 <= (uint32_t) y;
 }
