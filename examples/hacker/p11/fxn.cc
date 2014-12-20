c2a79d31cf865f4947d63dae82ba01c890353f5c
eric <eric.schkufza@gmail.com>
2014-12-19 17:34:42 -0800

    astyle

--- examples/hacker/p11/fxn.cc
+++ examples/hacker/p11/fxn.cc
@@ -15,7 +15,7 @@
 #include <stdint.h>
 
 int32_t p11(int32_t x, int32_t y) {
-	  int32_t o1 = ~y;
-		  int32_t o2 = x & o1;
-			  return (uint32_t) o2 > (uint32_t) y;
+  int32_t o1 = ~y;
+  int32_t o2 = x & o1;
+  return (uint32_t) o2 > (uint32_t) y;
 }
