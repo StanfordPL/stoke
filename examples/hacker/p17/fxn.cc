0d3ea5c4f9d770c805d07f691771994756b29b68
eric <eric.schkufza@gmail.com>
2014-12-19 16:49:48 -0800

    p16 is in.

--- examples/hacker/p17/fxn.cc
+++ examples/hacker/p17/fxn.cc
@@ -15,9 +15,9 @@
 #include <stdint.h>
 
 int32_t p17(int32_t x) {
-	  int32_t o1 = x - 1;
-		  int32_t o2 = x | o1;
-			  int32_t o3 = o2 + 1;
-				  return o3 & x;
+  int32_t o1 = x - 1;
+  int32_t o2 = x | o1;
+  int32_t o3 = o2 + 1;
+  return o3 & x;
 }
 
