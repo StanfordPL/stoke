5d78df70f1722cc0c307a1198ca623b73a880b7b
eric <eric.schkufza@gmail.com>
2015-01-06 16:17:47 -0800

    astyle

--- tools/apps/stoke_benchmark_sandbox.cc
+++ tools/apps/stoke_benchmark_sandbox.cc
@@ -45,21 +45,21 @@ int main(int argc, char** argv) {
     Console::error(1) << "Target reads undefined variables, or leaves live_out undefined." << endl;
   }
 
-	size_t loc = 0;
-	size_t mds = 0;
-	size_t nd = 0;
-	for (auto i = target.reachable_begin(), ie = target.reachable_end(); i != ie; ++i) {
-		nd = max(nd, target.nesting_depth(*i));
-		for (auto j = target.instr_begin(*i), je = target.instr_end(*i); j != je; ++j) {
-			if (j->is_any_return()) {
-				continue;
-			}
-			loc++;
-			if (j->is_memory_dereference()) {
-				mds++;
-			}
-		}
-	}
+  size_t loc = 0;
+  size_t mds = 0;
+  size_t nd = 0;
+  for (auto i = target.reachable_begin(), ie = target.reachable_end(); i != ie; ++i) {
+    nd = max(nd, target.nesting_depth(*i));
+    for (auto j = target.instr_begin(*i), je = target.instr_end(*i); j != je; ++j) {
+      if (j->is_any_return()) {
+        continue;
+      }
+      loc++;
+      if (j->is_memory_dereference()) {
+        mds++;
+      }
+    }
+  }
 
   Console::msg() << "Sandbox::run()..." << endl;
 
@@ -70,11 +70,11 @@ int main(int argc, char** argv) {
   const auto dur = duration_cast<duration<double>>(steady_clock::now() - start);
   const auto rps = tcs.size() * benchmark_itr_arg / dur.count();
 
-	Console::msg() << "LOC:        " << loc << endl;
-	Console::msg() << "Loop Depth: " << nd << endl;
-	Console::msg() << "Derefs:     " << mds << endl;
-	Console::msg() << "Testcases:  " << tcs.size() << endl;
-	Console::msg() << "Iterations: " << benchmark_itr_arg.value() << endl;
+  Console::msg() << "LOC:        " << loc << endl;
+  Console::msg() << "Loop Depth: " << nd << endl;
+  Console::msg() << "Derefs:     " << mds << endl;
+  Console::msg() << "Testcases:  " << tcs.size() << endl;
+  Console::msg() << "Iterations: " << benchmark_itr_arg.value() << endl;
   Console::msg() << "Runtime:    " << dur.count() << " seconds" << endl;
   Console::msg() << "Throughput: " << rps << " / second" << endl;
 
