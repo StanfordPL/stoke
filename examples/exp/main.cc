8f231787eedb83138406226cc7451c764cef5be0
eric <eric.schkufza@gmail.com>
2015-01-13 18:02:04 -0800

    stylin

--- examples/exp/main.cc
+++ examples/exp/main.cc
@@ -1,3 +1,17 @@
+// Copyright 2013-2015 Eric Schkufza, Rahul Sharma, Berkeley Churchill, Stefan Heule
+//
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an AS IS BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <cstdlib>
 #include <stdint.h>
 
@@ -8,16 +22,16 @@ extern double exp(double val, uint64_t* st);
 using namespace std;
 
 int main(int argc, char** argv) {
-	const auto itr = atoi(argv[1]);
+  const auto itr = atoi(argv[1]);
 
-	const auto min = -2.6;
-	const auto max = 0.12;
-	const auto delta = (max - min)/itr;
+  const auto min = -2.6;
+  const auto max = 0.12;
+  const auto delta = (max - min)/itr;
 
-	for (double val = min; val <= max; val += delta) {
-		exp(val, sym_table);
-	}
-	exp(max, sym_table);
+  for (double val = min; val <= max; val += delta) {
+    exp(val, sym_table);
+  }
+  exp(max, sym_table);
 
-	return 0;
+  return 0;
 }
--- examples/exp/sym_table.h
+++ examples/exp/sym_table.h
@@ -1,25 +1,39 @@
+// Copyright 2013-2015 Eric Schkufza, Rahul Sharma, Berkeley Churchill, Stefan Heule
+//
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an AS IS BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef SYM_TABLE_H
 #define SYM_TABLE_H
 
 #include <stdint.h>
 
 uint64_t sym_table[] = {
-	0x3ff71547652b82fe, 0x3ff71547652b82fe,
-	0xbfe62e42fefa39ef, 0xbfe62e42fefa39ef,
-	0xbc7abc9e3b39803f, 0xbc7abc9e3b39803f,
-	0x3ff0000000000000, 0x3ff0000000000000,
-	0x000003ff000003ff, 0x0000000000000000,
-	0x3e21eed8eff8d898, 0x3e21eed8eff8d898,
-	0x3e5ae64567f544e4, 0x3e5ae64567f544e4,
-	0x3e927e4fb7789f5c, 0x3e927e4fb7789f5c,
-	0x3ec71de3a556c733, 0x3ec71de3a556c733,
-	0x3efa01a01a01a01a, 0x3efa01a01a01a01a,
-	0x3f2a01a01a01a01a, 0x3f2a01a01a01a01a,
-	0x3f56c16c16c16c17, 0x3f56c16c16c16c17,
-	0x3f81111111111111, 0x3f81111111111111,
-	0x3fa5555555555554, 0x3fa5555555555554,
-	0x3fc5555555555557, 0x3fc5555555555557,
-	0x3fe0000000000000, 0x3fe0000000000000
+  0x3ff71547652b82fe, 0x3ff71547652b82fe,
+  0xbfe62e42fefa39ef, 0xbfe62e42fefa39ef,
+  0xbc7abc9e3b39803f, 0xbc7abc9e3b39803f,
+  0x3ff0000000000000, 0x3ff0000000000000,
+  0x000003ff000003ff, 0x0000000000000000,
+  0x3e21eed8eff8d898, 0x3e21eed8eff8d898,
+  0x3e5ae64567f544e4, 0x3e5ae64567f544e4,
+  0x3e927e4fb7789f5c, 0x3e927e4fb7789f5c,
+  0x3ec71de3a556c733, 0x3ec71de3a556c733,
+  0x3efa01a01a01a01a, 0x3efa01a01a01a01a,
+  0x3f2a01a01a01a01a, 0x3f2a01a01a01a01a,
+  0x3f56c16c16c16c17, 0x3f56c16c16c16c17,
+  0x3f81111111111111, 0x3f81111111111111,
+  0x3fa5555555555554, 0x3fa5555555555554,
+  0x3fc5555555555557, 0x3fc5555555555557,
+  0x3fe0000000000000, 0x3fe0000000000000
 };
 
 #endif
