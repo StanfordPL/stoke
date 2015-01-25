4ca80e0015240f6eb93f4eade465fdb2aed9944d
eric <eric.schkufza@gmail.com>
2015-01-25 00:11:15 -0800

    astyle

--- tools/apps/stoke_debug_sandbox.cc
+++ tools/apps/stoke_debug_sandbox.cc
@@ -42,128 +42,128 @@ auto& debug = FlagArg::create("debug")
 // The current program stack
 vector<pair<TUnit, size_t>> program_stack;
 
-// Kind of ugly; the callback needs to see this Gadgets, but we 
+// Kind of ugly; the callback needs to see this Gadgets, but we
 // can't make it global. It'll be initialized correctly in main.
 FunctionsGadget* fg = nullptr;
 
 void print_state(const StateCallbackData& data) {
-	Console::msg() << "Current State: " << endl;
-	Console::msg() << endl;
+  Console::msg() << "Current State: " << endl;
+  Console::msg() << endl;
   Console::msg() << data.state << endl;
   Console::msg() << endl;
 }
 
 void print_stack() {
-	Console::msg() << "Program Stack: " << endl;
-	Console::msg() << endl;
+  Console::msg() << "Program Stack: " << endl;
+  Console::msg() << endl;
 
   ofilterstream<Column> ofs(Console::msg());
   ofs.filter().padding(1);
 
-	for (size_t i = 0, ie = program_stack.size(); i < ie; ++i) {
-		ofs << "[" << i << "]" << endl;
-	}
-	ofs.filter().next();
-	for (size_t i = 0, ie = program_stack.size(); i < ie; ++i) {
-		ofs << program_stack[i].first.name << endl;
-	}
-	ofs.filter().next();
-	for (size_t i = 0, ie = program_stack.size(); i < ie; ++i) {
-		ofs << program_stack[i].second << endl;
-	}
+  for (size_t i = 0, ie = program_stack.size(); i < ie; ++i) {
+    ofs << "[" << i << "]" << endl;
+  }
+  ofs.filter().next();
+  for (size_t i = 0, ie = program_stack.size(); i < ie; ++i) {
+    ofs << program_stack[i].first.name << endl;
+  }
+  ofs.filter().next();
+  for (size_t i = 0, ie = program_stack.size(); i < ie; ++i) {
+    ofs << program_stack[i].second << endl;
+  }
 
-	ofs.filter().next();
-	ofs.filter().done();
+  ofs.filter().next();
+  ofs.filter().done();
 
-	Console::msg() << endl;
+  Console::msg() << endl;
 }
 
 void print_current(const pair<TUnit, size_t>& frame) {
-	const auto& instr = frame.first.code[frame.second];
+  const auto& instr = frame.first.code[frame.second];
   Console::msg() << "Current Instruction: " << instr << endl;
   Console::msg() << endl;
 }
 
 void print_frame(size_t idx, const pair<TUnit, size_t>& frame) {
-	Console::msg() << "[" << idx << "] " << frame.first.name << " " << frame.second << endl;
-	Console::msg() << endl;
-	for (size_t i = 0, ie = frame.first.code.size(); i < ie; ++i) {
-		Console::msg() << (i == frame.second ? "-> " : "   ") << frame.first.code[i] << endl;
-	}
-	Console::msg() << endl;
+  Console::msg() << "[" << idx << "] " << frame.first.name << " " << frame.second << endl;
+  Console::msg() << endl;
+  for (size_t i = 0, ie = frame.first.code.size(); i < ie; ++i) {
+    Console::msg() << (i == frame.second ? "-> " : "   ") << frame.first.code[i] << endl;
+  }
+  Console::msg() << endl;
 }
 
 bool user_loop() {
-	auto key = ' ';
-	auto idx = program_stack.size()-1;
+  auto key = ' ';
+  auto idx = program_stack.size()-1;
 
-	while (true) {
-		Console::msg() << "(l)ist, (u)p, (d)own, (s)tep, (c)ontinue, or (q)uit: ";
-		cin >> key;
-		Console::msg() << endl;
+  while (true) {
+    Console::msg() << "(l)ist, (u)p, (d)own, (s)tep, (c)ontinue, or (q)uit: ";
+    cin >> key;
+    Console::msg() << endl;
 
-		switch (key) {
-			case 'u':
-				idx = idx == 0 ? idx : idx-1;
-				print_frame(idx, program_stack[idx]);
-				break;
-			case 'd':
-				idx = idx == (program_stack.size()-1) ? idx : idx+1;
-				print_frame(idx, program_stack[idx]);
-				break;
-			case 'l':
-				print_frame(idx, program_stack[idx]);
-				break;
-			case 's':
-				return true;
-			case 'c':
-				return false;
-			case 'q':
-				exit(0);
-			default:
-				break;
-		}
-	}
+    switch (key) {
+    case 'u':
+      idx = idx == 0 ? idx : idx-1;
+      print_frame(idx, program_stack[idx]);
+      break;
+    case 'd':
+      idx = idx == (program_stack.size()-1) ? idx : idx+1;
+      print_frame(idx, program_stack[idx]);
+      break;
+    case 'l':
+      print_frame(idx, program_stack[idx]);
+      break;
+    case 's':
+      return true;
+    case 'c':
+      return false;
+    case 'q':
+      exit(0);
+    default:
+      break;
+    }
+  }
 }
 
 void update_stack(const pair<TUnit, size_t>& frame) {
-	const auto& instr = frame.first.code[frame.second];
-	if (instr.get_opcode() == CALL_LABEL) {
-		const auto dest = instr.get_operand<Label>(0);
-		if (dest == target_arg.value().code[0].get_operand<Label>(0)) {
-			program_stack.push_back({target_arg.value(), 0});
-		} else {
-			for (const auto& fxn : *fg) {
-				if (dest == fxn.code[0].get_operand<Label>(0)) {
-					program_stack.push_back({fxn, 0});
-				}
-			}
-		}
-	} else if (instr.is_any_return()) {
-		program_stack.pop_back();
-	}
+  const auto& instr = frame.first.code[frame.second];
+  if (instr.get_opcode() == CALL_LABEL) {
+    const auto dest = instr.get_operand<Label>(0);
+    if (dest == target_arg.value().code[0].get_operand<Label>(0)) {
+      program_stack.push_back({target_arg.value(), 0});
+    } else {
+      for (const auto& fxn : *fg) {
+        if (dest == fxn.code[0].get_operand<Label>(0)) {
+          program_stack.push_back({fxn, 0});
+        }
+      }
+    }
+  } else if (instr.is_any_return()) {
+    program_stack.pop_back();
+  }
 }
 
 void callback(const StateCallbackData& data, void* arg) {
-	assert(fg != nullptr);
-	assert(!program_stack.empty());
+  assert(fg != nullptr);
+  assert(!program_stack.empty());
 
-	auto& frame = program_stack.back();
-	frame.second = data.line;
+  auto& frame = program_stack.back();
+  frame.second = data.line;
 
-	// Print current execution state
-	print_state(data);
-	print_stack();
-	print_current(frame);
+  // Print current execution state
+  print_state(data);
+  print_stack();
+  print_current(frame);
 
-	// User interaction loop
+  // User interaction loop
   auto stepping = (bool*) arg;
-	if (*stepping) {
-		*stepping = user_loop();
-	}
+  if (*stepping) {
+    *stepping = user_loop();
+  }
 
-	// Update the stack based on the current instruction
-	update_stack(frame);
+  // Update the stack based on the current instruction
+  update_stack(frame);
 }
 
 int main(int argc, char** argv) {
@@ -183,9 +183,9 @@ int main(int argc, char** argv) {
   CpuStates tcs;
   tcs.push_back(tc);
 
-	auto stepping = debug.value();
-	fg = &aux_fxns;
-	program_stack.push_back({target_arg.value(), 0});
+  auto stepping = debug.value();
+  fg = &aux_fxns;
+  program_stack.push_back({target_arg.value(), 0});
   SandboxGadget sb(tcs, aux_fxns, {callback, &stepping});
 
   sb.run(target);
