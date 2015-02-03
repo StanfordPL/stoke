078243aa921781de3877373997a49a11f50f8878
eric <eric.schkufza@gmail.com>
2015-02-03 11:15:06 -0800

    Fixes assertion

--- src/cfg/cfg.h
+++ src/cfg/cfg.h
@@ -327,7 +327,7 @@ public:
 
   /** Returns the set of registers that are live-in to this graph. */
   x64asm::RegSet live_ins() const {
-    return live_ins(get_entry());
+    return live_ins(get_entry()+1);
   }
   /** Returns the set of registers that are live-in on entry a basic block. */
   x64asm::RegSet live_ins(id_type id) const {
