3d2a2d1af1cc4a9e1782ad3b461e5b53adbadeae
Berkeley Churchill (on mrwhite) <berkeley@berkeleychurchill.com>
2015-02-06 00:50:59 -0800

    hopefully got benchmark script right

--- tools/apps/stoke_benchmark_cfg.cc
+++ tools/apps/stoke_benchmark_cfg.cc
@@ -45,6 +45,7 @@ int main(int argc, char** argv) {
   const auto dur = duration_cast<duration<double>>(steady_clock::now() - start);
   const auto rps = benchmark_itr_arg / dur.count();
 
+  Console::msg() << fixed;
   Console::msg() << "Runtime:    " << dur.count() << " seconds" << endl;
   Console::msg() << "Throughput: " << rps << " / second" << endl;
 
