1842222d51183e5eeaaa44a9504c70b7d4990990
Berkeley Churchill (on mrwhite) <berkeley@berkeleychurchill.com>
2015-02-16 15:31:11 -0800

    work on testing

--- tests/cost/cost.h
+++ tests/cost/cost.h
@@ -12,5 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #include "tests/cost/correctness.h"
+#include "tests/cost/latency.h"
+
