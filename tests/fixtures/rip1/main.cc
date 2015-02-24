c9ce5e5876aef53cb0fe3f4467e5933003c7b93d
eric <eric.schkufza@gmail.com>
2015-02-24 11:06:38 -0800

    Simple rip testcase.

---
+++ tests/fixtures/rip1/main.cc
@@ -0,0 +1,25 @@
+// Copyright 2013-2015 Stanford University
+//
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an AS IS BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Simple test: Can stoke find the code that moves the rip constant into xmm0?
+
+double foo() {
+  double x = 5.234;
+  return x;
+}
+
+int main(int argc, char** argv) {
+  const auto x = foo();
+  return x > 10.0;
+}
