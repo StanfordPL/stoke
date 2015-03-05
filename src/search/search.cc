f814aa70adfd7d45ebafb0f40f2376b8208f2fb8
eric <eric.schkufza@gmail.com>
2015-03-04 17:50:23 -0800

    astyle

--- src/search/search.cc
+++ src/search/search.cc
@@ -187,20 +187,20 @@ void Search::configure(const Cfg& target, CostFunction& fxn, SearchState& state,
   state.best_yet.recompute();
   state.best_correct.recompute();
 
-	// add dataflow information about function call targets
-	for (const auto& fxn : aux_fxn) {
-		const auto& code = fxn.get_code();
-		const auto& lbl = fxn.get_leading_label();
-		TUnit::MayMustSets mms = {
-			code.must_read_set(),
-			code.must_write_set(),
-			code.must_undef_set(),
-			code.maybe_read_set(),
-			code.maybe_write_set(),
-			code.maybe_undef_set()
-		};
-		state.current.add_summary(lbl, fxn.get_may_must_sets(mms));
-	}
+  // add dataflow information about function call targets
+  for (const auto& fxn : aux_fxn) {
+    const auto& code = fxn.get_code();
+    const auto& lbl = fxn.get_leading_label();
+    TUnit::MayMustSets mms = {
+      code.must_read_set(),
+      code.must_write_set(),
+      code.must_undef_set(),
+      code.maybe_read_set(),
+      code.maybe_write_set(),
+      code.maybe_undef_set()
+    };
+    state.current.add_summary(lbl, fxn.get_may_must_sets(mms));
+  }
 
   state.current_cost = fxn(state.current).second;
   state.best_yet_cost = fxn(state.best_yet).second;
