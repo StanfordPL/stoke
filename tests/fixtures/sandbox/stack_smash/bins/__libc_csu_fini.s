37306ab5d701809d810151cef6f4debee3dd6b1c
eric <eric.schkufza@gmail.com>
2015-03-06 15:07:17 -0800

    Stack smash code and error.

---
+++ tests/fixtures/sandbox/stack_smash/bins/__libc_csu_fini.s
@@ -0,0 +1,14 @@
+  .text
+  .globl __libc_csu_fini
+  .type __libc_csu_fini, @function
+
+#! file-offset 0x580
+#! rip-offset  0x400580
+#! capacity    2 bytes
+
+# Text             #  Line  RIP       Bytes  
+.__libc_csu_fini:  #        0x400580  0      
+  retq             #  1     0x400580  1      
+                                             
+.size __libc_csu_fini, .-__libc_csu_fini
+
