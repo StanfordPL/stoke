a92faae028cba2b14cc732e06a2548c047c4f8a4
Berkeley Churchill (on mrwhite) <berkeley@berkeleychurchill.com>
2015-05-31 20:03:05 -0700

    Closes #643

--- scripts/make/submodule-init.sh
+++ scripts/make/submodule-init.sh
@@ -3,5 +3,5 @@
 # if the path does not exist, then we initialize that submodule
 if [ ! -f "$1/.git" ]; then
   echo "Updating submodule $1..."
-  git submodule update --init --recursive "$1"
+#  git submodule update --init --recursive "$1"
 fi
