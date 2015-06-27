1a7056572dd55da4275f992d43a844ad9392903d
Stefan Heule <stefanheule@gmail.com>
2015-06-27 16:05:37 -0700

    finish nongoal feature

--- tools/gadgets/cost_function.h
+++ tools/gadgets/cost_function.h
@@ -56,7 +56,7 @@ private:
     st["measured"] =     new MeasuredCost();
     st["size"] =         new SizeCost();
     st["sseavx"] =       new SseAvxCost();
-    st["nongoal"] =      new NonGoalCostGadget();
+    st["nongoal"] =      new NonGoalCostGadget(target.def_ins(), target.live_outs());
 
     CostParser cost_p(cost_function_arg.value(), st);
     auto cost_fxn = cost_p.run();
