d1677c60fc5cb8c794e838bc3887be48a6da209f
Berkeley Churchill (on mrwhite) <berkeley@berkeleychurchill.com>
2015-09-21 13:55:22 -0700

    updated wmemcmp binaries

--- examples/nacl_shootout/web-shootout.googlecode.com/git/nacl/bench-framework.c
+++ examples/nacl_shootout/web-shootout.googlecode.com/git/nacl/bench-framework.c
@@ -194,10 +194,10 @@ void SetupLargeBenchmarks() {
   //RegisterBenchmark(strdup("wcscpy"), run_wcscpy, 1000000, 30000000, NULL, NULL);
   //RegisterBenchmark(strdup("strxfrm"), run_strxfrm, 10000000, 30000000, NULL, NULL);
   //RegisterBenchmark(strdup("strlen"), run_strlen, 1000000000, 30000000, NULL, NULL);
-  //RegisterBenchmark(strdup("wmemcmp"), run_wmemcmp, 10000000, 30000000, NULL, NULL);
+  RegisterBenchmark(strdup("wmemcmp"), run_wmemcmp, 10000000, 30000000, NULL, NULL);
   //RegisterBenchmark(strdup("wcscmp"), run_wcscmp, 5000000, 30000000, NULL, NULL);
   //RegisterBenchmark(strdup("wmemchr"), run_wmemchr, 5000000, 30000000, NULL, NULL);
-  RegisterBenchmark(strdup("wmemset"), run_wmemset, 5000000, 30000000, NULL, NULL);
+  //RegisterBenchmark(strdup("wmemset"), run_wmemset, 5000000, 30000000, NULL, NULL);
   //RegisterBenchmark(strdup("memchr"), run_memchr, 5000000, 30000000, NULL, NULL);
   //RegisterBenchmark(strdup("wcschr"), run_wcschr, 5000000, 30000000, NULL, NULL);
   //RegisterBenchmark(strdup("wcsrchr"), run_wcsrchr, 5000000, 30000000, NULL, NULL);
