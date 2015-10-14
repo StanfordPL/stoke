0678036f39352b330265141784c73f28f172d4f7
Stefan Heule <stefanheule@gmail.com>
2015-10-13 17:41:29 -0700

    work in progress

--- tools/apps/specgen_circuit.cc
+++ tools/apps/specgen_circuit.cc
@@ -39,11 +39,16 @@
 #include "tools/apps/specgen.h"
 #include "tools/apps/support.h"
 
+#define BOOST_NO_CXX11_SCOPED_ENUMS
+#include <boost/filesystem.hpp>
+
 using namespace cpputil;
 using namespace std;
 using namespace stoke;
 using namespace x64asm;
 using namespace std::chrono;
+using namespace boost;
+using namespace boost::filesystem;
 
 auto& io_opt = Heading::create("Main option:");
 
@@ -76,13 +81,18 @@ auto& code_arg = ValueArg<Code, CodeReader, CodeWriter>::create("code")
                  .description("Input code directly");
 
 
-const auto formula_dir = string(getenv("HOME")) + "/dev/formulas";
+const auto circuit_dir = string(getenv("HOME")) + "/dev/circuits/";
 
+void print_state(RegSet regs, SymState& state);
 
 void build_circuit(const x64asm::Instruction& instr, SymState& start) {
   ComboHandler ch;
 
   auto opcode = instr.get_opcode();
+  stringstream ss;
+  ss << opcode;
+  auto opcode_str = ss.str();
+  auto candidate_file = circuit_dir + opcode_str + ".s";
 
   // base instruction: use handwritten formula
   if (find(instr_cat_base_.begin(), instr_cat_base_.end(), opcode) != instr_cat_base_.end()) {
@@ -93,60 +103,47 @@ void build_circuit(const x64asm::Instruction& instr, SymState& start) {
     return;
   }
 
+  // we have a program for this instruction
+  else if (filesystem::exists(candidate_file)) {
+    // read program
+    ifstream file(candidate_file);
+    TUnit t;
+    file >> t;
+    auto specgen_instr = get_instruction(opcode);
+
+    // build formula for program
+    SymState tmp(opcode_str);
+    auto code = t.get_code();
+    assert(code[0].get_opcode() == Opcode::LABEL_DEFN);
+    assert(code[code.size() - 1].get_opcode() == Opcode::RET);
+    for (size_t i = 1; i < code.size()-1; i++) {
+      build_circuit(code[i], tmp);
+    }
+
+    cout << "--------------" << endl << endl;
+    print_state(specgen_instr.maybe_write_set(), tmp);
+    cout << "--------------" << endl << endl;
+
+    // rename variables in the tmp state to the values in start
+
+    // update the start state with the circuits from tmp
+    
+  }
+
   // unknown instruction
   else {
     Console::error() << "Unknown instruction '" << instr << "'." << endl;
   }
 }
 
-int main(int argc, char** argv) {
-  // not actually required here
-  target_arg.required(false);
-
-  cout << formula_dir << endl;
-
-  CommandLineConfig::strict_with_convenience(argc, argv);
-
-  if (!code_arg.has_been_provided() && !target_arg.has_been_provided()) {
-    Console::error() << "Either --code or --target required." << endl;
-  }
-
-  if (code_arg.has_been_provided() && target_arg.has_been_provided()) {
-    Console::error() << "At most one of --code and --target can be provided." << endl;
-  }
-
-  FunctionsGadget aux_fxns;
-  TargetGadget target(aux_fxns, false);
-  Code code = target.get_code();
-  if (code_arg.has_been_provided()) {
-    code = code_arg.value();
-  }
-  auto instr = code.size() > 1 ? code[1] : code[0];
-
-  Console::msg() << "Instruction:       " << instr << endl;
-  Console::msg() << "Opcode:            " << instr.get_opcode() << endl;
-  Console::msg() << "Maybe written to:  " << instr.maybe_write_set() << endl;
-  Console::msg() << endl;
-
-  SymState state("", true);
-  build_circuit(instr, state);
-
-  Console::msg() << "Circuits:" << endl;
-  Console::msg() << endl;
-
+void print_state(RegSet regs, SymState& state) {
   SymPrettyVisitor print(Console::msg());
 
   // print symbolic state
   bool printed = false;
   auto pad = 7;
 
-  RegSet rs = (RegSet::all_gps() | RegSet::all_ymms()) +
-              Constants::eflags_cf() +
-              Constants::eflags_sf() +
-              Constants::eflags_zf() +
-              Constants::eflags_of() +
-              Constants::eflags_pf();
-  rs = instr.maybe_write_set();
+  RegSet rs = regs;
   for (auto gp_it = rs.gp_begin(); gp_it != rs.gp_end(); ++gp_it) {
     auto fullreg = Constants::r64s()[*gp_it];
     if ((*gp_it).type() == Type::RH) {
@@ -192,4 +189,40 @@ int main(int argc, char** argv) {
   Console::msg() << "sigsegv:     ";
   print(state.sigsegv);
   Console::msg() << endl;
+}
+
+int main(int argc, char** argv) {
+  // not actually required here
+  target_arg.required(false);
+
+  CommandLineConfig::strict_with_convenience(argc, argv);
+
+  if (!code_arg.has_been_provided() && !target_arg.has_been_provided()) {
+    Console::error() << "Either --code or --target required." << endl;
+  }
+
+  if (code_arg.has_been_provided() && target_arg.has_been_provided()) {
+    Console::error() << "At most one of --code and --target can be provided." << endl;
+  }
+
+  FunctionsGadget aux_fxns;
+  TargetGadget target(aux_fxns, false);
+  Code code = target.get_code();
+  if (code_arg.has_been_provided()) {
+    code = code_arg.value();
+  }
+  auto instr = code.size() > 1 ? code[1] : code[0];
+
+  Console::msg() << "Instruction:       " << instr << endl;
+  Console::msg() << "Opcode:            " << instr.get_opcode() << endl;
+  Console::msg() << "Maybe written to:  " << instr.maybe_write_set() << endl;
+  Console::msg() << endl;
+
+  SymState state("", true);
+  build_circuit(instr, state);
+
+  Console::msg() << "Circuits:" << endl;
+  Console::msg() << endl;
+
+  print_state(instr.maybe_write_set(), state);
 }
