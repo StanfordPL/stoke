98f1d2f9336471821404dec954e3401e2c3af368
Stefan Heule <stefanheule@gmail.com>
2015-10-17 22:26:30 -0700

    add circuits for vfmadd132ss and vfmadd132sd

--- src/validator/handlers/simple_handler.cc
+++ src/validator/handlers/simple_handler.cc
@@ -602,6 +602,20 @@ void SimpleHandler::add_all() {
     }
   });
 
+  add_opcode({"vfmadd132ss"},
+  [this] (Operand dst, Operand src1, Operand src2, SymBitVector a, SymBitVector b, SymBitVector c, SymState& ss) {
+    SymFunction f("vfmadd132_single", 32, {32, 32});
+    auto res = f(b[31][0], c[31][0]);
+    ss.set(dst, SymBitVector::constant(128, 0) || ss[dst][127][32] || res, true);
+  });
+
+  add_opcode({"vfmadd132sd"},
+  [this] (Operand dst, Operand src1, Operand src2, SymBitVector a, SymBitVector b, SymBitVector c, SymState& ss) {
+    SymFunction f("vfmadd132_double", 64, {64, 64});
+    auto res = f(b[63][0], c[63][0]);
+    ss.set(dst, SymBitVector::constant(128, 0) || ss[dst][127][64] || res, true);
+  });
+
 }
 
 Handler::SupportLevel SimpleHandler::get_support(const x64asm::Instruction& instr) {
