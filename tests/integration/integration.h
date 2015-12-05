844072ba64610666bc60909a3c8b666532d27da1
Stefan Heule <stefanheule@gmail.com>
2015-12-05 14:36:07 -0800

    disable bansal test

--- tests/integration/integration.h
+++ tests/integration/integration.h
@@ -185,20 +185,21 @@ TEST_F(IntegrationTest, ExamplesExp) {
 }
 #endif
 
-TEST_F(IntegrationTest, ExamplesBansal) {
-  set_working_dir("examples/bansal");
-  set_path("../../bin");
+// Disabled for now, see #788, because it is too slow in debug mode
+// TEST_F(IntegrationTest, ExamplesBansal) {
+//   set_working_dir("examples/bansal");
+//   set_path("../../bin");
 
-  // In 10 tries, search should succeed at least once...
-  bool good = false;
-  for (size_t i = 0; i < 10 && good == false; ++i) {
-    if (shell("make") == 0ull) {
-      good = true;
-    }
-    EXPECT_EQ(0ull, shell("make clean"));
-  }
-  EXPECT_EQ(good, true);
-}
+//   // In 10 tries, search should succeed at least once...
+//   bool good = false;
+//   for (size_t i = 0; i < 10 && good == false; ++i) {
+//     if (shell("make") == 0ull) {
+//       good = true;
+//     }
+//     EXPECT_EQ(0ull, shell("make clean"));
+//   }
+//   EXPECT_EQ(good, true);
+// }
 
 TEST_F(IntegrationTest, CostLiveOut) {
   set_working_dir("tests/fixtures/cost/live_out");
