36149ff3c15c23f9e33ffe5a8adea7042448d1ac
Stefan Heule <stefanheule@gmail.com>
2015-12-09 13:44:36 -0800

    work in progress

--- tests/x64asm/read_write_sets.h
+++ tests/x64asm/read_write_sets.h
@@ -191,6 +191,10 @@ TEST(X64AsmTest, SpreadsheetReadWriteSetFuzzTest) {
   tps.remove_opcode(x64asm::VPGATHERQQ_XMM_M64_XMM);
   tps.remove_opcode(x64asm::VPGATHERQQ_YMM_M64_YMM);
 
+    // see issue #791
+  tps.remove_opcode(x64asm::VEXTRACTI128_M128_YMM_IMM8);
+  tps.remove_opcode(x64asm::VEXTRACTI128_XMM_YMM_IMM8);
+
   // code to use if you want to test a single instruction only
   // for (size_t i = 0; i < X64ASM_NUM_OPCODES; ++i) {
   //   tps.remove_opcode((x64asm::Opcode)i);
