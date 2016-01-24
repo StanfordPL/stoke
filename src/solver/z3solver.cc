d6cbf1417746b6c8e59dc8b13012e2033b1c901b
Berkeley Churchill (on mrwhite) <berkeley@berkeleychurchill.com>
2016-01-24 04:26:49 -0800

    added more tests for flat memory model models; fixed bugs; all things pass

--- src/solver/z3solver.cc
+++ src/solver/z3solver.cc
@@ -177,24 +177,29 @@ std::map<uint64_t, cpputil::BitVector> Z3Solver::get_model_array(
   expr e = model_->eval(v, true);
   auto array_eval_func_decl = e.decl();
 
-/*
-  cout << *model_ << endl;
-*/
+  //cout << *model_ << endl;
 
   // CREDIT: this was written with A LOT of help from
   // https://stackoverflow.com/questions/22885457/read-func-interp-of-a-z3-array-from-the-z3-model
 
   bool ok = true;
   ok &= Z3_get_decl_kind(context_, array_eval_func_decl) == Z3_OP_AS_ARRAY; 
-  ok &= Z3_is_app(context_, array_eval_func_decl);
-  ok &= (Z3_get_decl_num_parameters(context_, array_eval_func_decl) == 1);
-  ok &= (Z3_get_decl_parameter_kind(context_, array_eval_func_decl, 0) == 
-         Z3_PARAMETER_FUNC_DECL);
+  /* These checks don't seem to work right
+    cout << "check1: ok=" << ok << endl;
+    ok &= Z3_is_app(context_, array_eval_func_decl);
+    cout << "check2: ok=" << ok << endl;
+    ok &= (Z3_get_decl_num_parameters(context_, array_eval_func_decl) == 1);
+    cout << "check3: ok=" << ok << endl;
+    ok &= (Z3_get_decl_parameter_kind(context_, array_eval_func_decl, 0) == 
+           Z3_PARAMETER_FUNC_DECL);
+    cout << "check4: ok=" << ok << endl;
+  */
 
   if(!ok) {
     // The counterexample could be spurious, but we'll figure that out later.
     // On the other hand, there might be no memory at all or the memory
     // does not matter
+    //cout << "got empty addr-value map" << endl;
     return addr_val_map;
   }
 
