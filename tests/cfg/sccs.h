cdf02043a967d8b522cb28c24ec89e6594457b64
Berkeley Churchill <berkc@stanford.edu>
2016-02-05 15:05:39 -0800

    improving Sccs tests (*)

--- tests/cfg/sccs.h
+++ tests/cfg/sccs.h
@@ -25,7 +25,63 @@
 
 namespace stoke {
 
-TEST(SccTest, NoSccs) {
+class SccTest : public ::testing::Test {
+
+public:
+  void SetUp() {
+
+  }
+
+  void check_consistency(Cfg& cfg, CfgSccs& sccs) {
+
+    // For each block, check that:
+    //   in_scc() matches get_scc()
+    //   check that it's in the appropriate vector for get_blocks()
+    //   check that it's not in any other get_blocks() vector
+    for(Cfg::id_type blk = cfg.get_entry(); blk != cfg.get_exit(); blk++) {
+      size_t the_scc = sccs.get_scc(blk);
+
+      ASSERT_EQ(the_scc == (size_t)(-1), !sccs.in_scc(blk));
+
+      if(!sccs.in_scc(blk)) {
+
+        for(size_t i = 0; i < sccs.count(); ++i) {
+          auto bad_list = sccs.get_blocks(i);
+          for(auto entry : bad_list) {
+            EXPECT_NE(entry, blk);
+          }
+        }
+
+        continue;
+      }
+
+      auto correct_list = sccs.get_blocks(the_scc);
+      size_t occurrences = 0;
+      for(auto entry : correct_list) {
+        if(entry == blk) {
+          occurrences++;
+        }
+      }
+      EXPECT_EQ(1ul, occurrences);
+
+      occurrences = 0;
+      for(size_t i = 0; i < sccs.count(); ++i) {
+        if(i != the_scc) {
+          auto bad_list = sccs.get_blocks(i);
+          for(auto entry : bad_list) {
+            if(entry == blk)
+              occurrences++;
+          }
+        }
+      }
+      EXPECT_EQ(0ul, occurrences);
+    }
+
+  }
+
+};
+
+TEST_F(SccTest, NoSccs) {
 
   std::stringstream ss;
   ss << ".foo:" << std::endl;
@@ -37,6 +93,7 @@ TEST(SccTest, NoSccs) {
 
   Cfg cfg(c);
   CfgSccs sccs(cfg);
+  check_consistency(cfg, sccs);
 
   EXPECT_EQ(0ul, sccs.count());
 
@@ -44,9 +101,10 @@ TEST(SccTest, NoSccs) {
     EXPECT_EQ(-1, sccs.get_scc(*it));
     EXPECT_FALSE(sccs.in_scc(*it));
   }
+
 }
 
-TEST(SccTest, SimpleLoop) {
+TEST_F(SccTest, SimpleLoop) {
 
   std::stringstream ss;
   ss << ".foo:" << std::endl;
@@ -59,6 +117,7 @@ TEST(SccTest, SimpleLoop) {
 
   Cfg cfg(c);
   CfgSccs sccs(cfg);
+  check_consistency(cfg, sccs);
 
   EXPECT_EQ(1ul, sccs.count());
 
@@ -71,9 +130,12 @@ TEST(SccTest, SimpleLoop) {
       EXPECT_FALSE(sccs.in_scc(*it));
     }
   }
+
+  ASSERT_EQ(1ul, sccs.get_blocks(0).size());
+  EXPECT_EQ(1ul, sccs.get_blocks(0)[0]);
 }
 
-TEST(SccTest, NestedLoop) {
+TEST_F(SccTest, NestedLoop) {
 
   std::stringstream ss;
   ss << ".foo:" << std::endl;            // BB1
@@ -94,6 +156,7 @@ TEST(SccTest, NestedLoop) {
 
   Cfg cfg(c);
   CfgSccs sccs(cfg);
+  check_consistency(cfg, sccs);
 
   EXPECT_EQ(1ul, sccs.count());
 
@@ -106,9 +169,15 @@ TEST(SccTest, NestedLoop) {
       EXPECT_FALSE(sccs.in_scc(*it));
     }
   }
+
+  ASSERT_EQ(4ul, sccs.get_blocks(0).size());
+  for(size_t i = 0; i < 4; ++i) {
+    EXPECT_LE(1ul, sccs.get_blocks(0)[i]);
+    EXPECT_GE(4ul, sccs.get_blocks(0)[i]);
+  }
 }
 
-TEST(SccTest, TwoLoops) {
+TEST_F(SccTest, TwoLoops) {
 
   std::stringstream ss;
   ss << ".foo:" << std::endl;            // BB1
@@ -128,6 +197,7 @@ TEST(SccTest, TwoLoops) {
 
   Cfg cfg(c);
   CfgSccs sccs(cfg);
+  check_consistency(cfg, sccs);
 
   EXPECT_EQ(2ul, sccs.count());
 
@@ -143,9 +213,13 @@ TEST(SccTest, TwoLoops) {
       EXPECT_FALSE(sccs.in_scc(*it));
     }
   }
+
+  ASSERT_EQ(2ul, sccs.get_blocks(1).size());
+  ASSERT_EQ(1ul, sccs.get_blocks(0).size());
+  EXPECT_EQ(4ul, sccs.get_blocks(0)[0]);
 }
 
-TEST(SccTest, TwoLoopsWcslen) {
+TEST_F(SccTest, TwoLoopsWcslen) {
 
   std::stringstream ss;
   ss << ".wcslen:" << std::endl;        // BB1
@@ -168,6 +242,7 @@ TEST(SccTest, TwoLoopsWcslen) {
 
   Cfg cfg(c);
   CfgSccs sccs(cfg);
+  check_consistency(cfg, sccs);
 
   EXPECT_EQ(1ul, sccs.count());
 
@@ -180,6 +255,79 @@ TEST(SccTest, TwoLoopsWcslen) {
       EXPECT_FALSE(sccs.in_scc(*it));
     }
   }
+}
+
+TEST_F(SccTest, Wcsrchr) {
+
+  std::stringstream ss;
+  // BB1
+  ss << ".wcsrchr:" << endl;
+  ss << "  movl %edi, %edi" << endl;
+  ss << "  movl %edi, %edi" << endl;
+  ss << "  movl (%r15,%rdi,1), %ecx" << endl;
+  ss << "  movq %rdi, %rax" << endl;
+  ss << "  testl %ecx, %ecx" << endl;
+  ss << "  je .L_143900" << endl;
+  // BB2
+  ss << "  xchgw %ax, %ax" << endl;
+  // BB3
+  ss << ".L_1438e0:" << endl;
+  ss << "  addl $0x4, %eax" << endl;
+  ss << "  movl %eax, %eax" << endl;
+  ss << "  movl (%r15,%rax,1), %edx" << endl;
+  ss << "  testl %edx, %edx" << endl;
+  ss << "  jne .L_1438e0" << endl;
+  // BB4
+  ss << "  cmpl %eax, %edi" << endl;
+  ss << "  ja .L_143960" << endl;
+  // BB5
+  ss << ".L_143900:" << endl;
+  ss << "  testl %esi, %esi" << endl;
+  ss << "  jne .L_143940" << endl;
+  // BB6
+  ss << "  jmpq .L_1439a0" << endl;
+  // BB7
+  ss << ".L_143920:" << endl;
+  ss << "  movl %eax, %eax" << endl;
+  ss << "  cmpl %esi, (%r15,%rax,1)" << endl;
+  ss << "  je .L_143980" << endl;
+  // BB8
+  ss << ".L_143940:" << endl;
+  ss << "  subl $0x4, %eax" << endl;
+  ss << "  cmpl %eax, %edi" << endl;
+  ss << "  jbe .L_143920" << endl;
+  // BB9
+  ss << ".L_143960:" << endl;
+  ss << "  xorl %eax, %eax" << endl;
+  // BB10
+  ss << ".L_143980:" << endl;
+  ss << "  retq" << endl;
+  // BB11
+  ss << ".L_1439a0:" << endl;
+  ss << "  retq" << endl;
+
+  x64asm::Code c;
+  ss >> c;
+
+  Cfg cfg(c);
+  CfgSccs sccs(cfg);
+  check_consistency(cfg, sccs);
+
+  EXPECT_TRUE(sccs.in_scc(3));
+  EXPECT_TRUE(sccs.in_scc(7));
+  EXPECT_TRUE(sccs.in_scc(8));
+
+  // Ensure get_scc() make sense
+  EXPECT_NE(sccs.get_scc(3), sccs.get_scc(7));
+  EXPECT_EQ(sccs.get_scc(7), sccs.get_scc(8));
+
+  // Ensure others are not in an SCC
+  for(size_t i = 0; i < 11; ++i) {
+    if(i != 3 && i != 7 && i != 8) {
+      EXPECT_FALSE(sccs.in_scc(i)) << "Found block " << i << " in SCC " << sccs.get_scc(i) << "; should not be." << endl;
+    }
+  }
+
 }
 
 
