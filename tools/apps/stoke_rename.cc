0ac920cb05d037c00519dd7978f26ae9206020aa
Berkeley Churchill <bchurchill@vmware.com>
2016-07-26 15:10:21 -0700

    improvements, bug fixes to renamer

--- tools/apps/stoke_rename.cc
+++ tools/apps/stoke_rename.cc
@@ -38,6 +38,8 @@ using namespace x64asm;
 
 auto& verbose = FlagArg::create("verbose");
 
+auto& no_verify = FlagArg::create("no_verify");
+
 auto& output = ValueArg<string>::create("output")
                .description("file to write rewritten file to (or original, if verification fails)")
                .default_val("");
@@ -56,12 +58,37 @@ public:
 
   /** Initialize map to undefined. */
   RenameMap() {
-    next_gp = 0;
-    next_sse = 0;
     for (size_t i = 0; i < 16; ++i) {
       gp_map[i] = -1;
       sse_map[i] = -1;
     }
+  }
+
+  /** Finish the mapping, leaving as many registers as possible mapping to
+   * themslves. */
+  void beautify() {
+
+    vector<map<size_t, size_t>*> ms;
+    ms.push_back(&this->gp_map);
+    ms.push_back(&this->sse_map);
+    for (map<size_t, size_t>* m : ms) {
+      for (size_t i = 0; i < 16; ++i) {
+        if (m->at(i) == (uint64_t)(-1)) {
+          if (!index_taken(*m, i))
+            (*m)[i] = i;
+        }
+      }
+    }
+
+
+    for (map<size_t, size_t>* m : ms) {
+      for (size_t i = 0; i < 16; ++i) {
+        if (m->at(i) == (uint64_t)(-1)) {
+          (*m)[i] = get_free_index(*m);
+        }
+      }
+    }
+
   }
 
   /** Apply the mapping to a code segment. */
@@ -83,11 +110,32 @@ public:
     return instr;
   }
 
+  /** Preprocess a code segment */
+  void preprocess(const Code& code) {
+    for (size_t i = 0; i < code.size(); ++i) {
+      preprocess(code[i]);
+    }
+  }
+
+  /** Preprocess an instruction to learn constraints. */
+  void preprocess(const Instruction& instr) {
+    auto implicit = instr.implicit_maybe_read_set() |
+                    instr.implicit_maybe_write_set() | instr.implicit_maybe_undef_set();
+    for (auto r = implicit.gp_begin(); r != implicit.gp_end(); ++r) {
+      uint64_t index = (uint64_t)(*r);
+      gp_map[index] = index;
+    }
+    for (auto r = implicit.sse_begin(); r != implicit.sse_end(); ++r) {
+      uint64_t index = (uint64_t)(*r);
+      sse_map[index] = index;
+    }
+  }
+
   /** Here's where the real work happens.*/
   R operator()(R& r) {
     uint64_t index = (uint64_t)(r);
     if (gp_map[index] == (uint64_t)(-1)) {
-      gp_map[index] = next_gp++;
+      gp_map[index] = get_free_gp_index();
     }
     index = gp_map[index];
 
@@ -112,7 +160,7 @@ public:
     } else if (op.is_sse_register()) {
       uint64_t index = (uint64_t)(reinterpret_cast<Sse&>(op));
       if (sse_map[index] == (uint64_t)(-1)) {
-        sse_map[index] = next_sse++;
+        sse_map[index] = get_free_sse_index();
       }
       index = sse_map[index];
       switch (op.size()) {
@@ -161,8 +209,29 @@ public:
 
 private:
 
-  size_t next_gp;
-  size_t next_sse;
+  size_t get_free_gp_index() {
+    return get_free_index(this->gp_map);
+  }
+
+  size_t get_free_sse_index() {
+    return get_free_index(this->sse_map);
+  }
+
+  bool index_taken(map<size_t, size_t>& m, size_t n) {
+    for (size_t i = 0; i < 16; ++i) {
+      if (m.at(i) == n)
+        return true;
+    }
+    return false;
+  }
+
+  size_t get_free_index(map<size_t,size_t>& m) {
+    for (size_t i = 0; i < 16; ++i) {
+      if (!index_taken(m, i))
+        return i;
+    }
+    return -1;
+  }
 
   map<size_t, size_t> gp_map;
   map<size_t, size_t> sse_map;
@@ -224,49 +293,80 @@ int main(int argc, char** argv) {
   TargetGadget target(aux_fxns, false);
   SolverGadget solver;
   ObligationChecker obc(solver);
+  obc.set_alias_strategy(ObligationChecker::AliasStrategy::FLAT);
+
+  // Build the mapping and rename registers
 
   RenameMap rm;
-  auto rewrite_code = rm(target.get_code());
+  auto target_code = target.get_code();
+
+  rm.preprocess(target_code);
+  auto rewrite_code = rm(target_code);
   Cfg rewrite(rewrite_code, target.def_ins(), target.live_outs());
 
-
-  auto target_paths = CfgPaths::enumerate_paths(target, 1, target.get_entry(), target.get_exit());
-  auto rewrite_paths = CfgPaths::enumerate_paths(target, 1, target.get_entry(), target.get_exit());
-
-  auto invariant = build_invariant(rm, RegSet::universe());
+  rm.beautify();
 
   if (verbose.value()) {
-    Console::msg() << "Rewrite" << endl << endl << rewrite.get_function() << endl;
+    Console::msg() << "********************************************************************" << endl << endl;
+    Console::msg() << "Original" << endl << endl << target.get_function() << endl << endl;
+    Console::msg() << "Rewrite" << endl << endl << rewrite.get_function() << endl << endl;
 
     cout << "MAPPING" << endl;
     rm.print();
     cout << endl;
-
-    cout << "INVARIANT: " << *invariant << endl;
   }
 
-  bool verify_success = true;
-  for (auto tp : target_paths) {
-    for (auto rp : rewrite_paths) {
-      auto res = obc.check(static_cast<Cfg&>(target),
-                           rewrite, tp, rp, *invariant, *invariant);
+  // Invoke the verifier
+  if (!no_verify.value()) {
+    auto target_paths = CfgPaths::enumerate_paths(target, 1, target.get_entry(), target.get_exit());
+    auto rewrite_paths = CfgPaths::enumerate_paths(target, 1, target.get_entry(), target.get_exit());
 
-      if (!res) {
-        verify_success = false;
-        break;
+    auto invariant = build_invariant(rm, RegSet::universe());
+
+    if (verbose.value())
+      cout << "INVARIANT: " << *invariant << endl;
+
+    bool verify_success = true;
+    try {
+      for (auto tp : target_paths) {
+        for (auto rp : rewrite_paths) {
+          auto res = obc.check(static_cast<Cfg&>(target),
+                               rewrite, tp, rp, *invariant, *invariant);
+
+          if (!res) {
+            verify_success = false;
+
+            if (verbose.value() && obc.checker_has_ceg()) {
+              cout << "Counterexample for original/rewrite start states" << endl;
+              cout << "Original:" << endl;
+              cout << obc.checker_get_target_ceg() << endl << endl;
+              cout << "Rewrite: " << endl;
+              cout << obc.checker_get_target_ceg() << endl;
+              cout << endl << endl;
+            }
+
+            break;
+          }
+        }
+      }
+    } catch (validator_error e) {
+      verify_success = false;
+      if (verbose.value()) {
+        Console::msg() << "Validation error: " << e.get_message() << endl;
+      }
+    }
+
+    if (verbose.value()) {
+      if (verify_success) {
+        Console::msg() << "Verified!" << endl;
+      } else {
+        Console::msg() << "Verification failed!" << endl;
+        rewrite = target;
       }
     }
   }
 
-  if (verbose.value()) {
-    if (verify_success) {
-      Console::msg() << "Verified!" << endl;
-    } else {
-      Console::msg() << "Verification failed!" << endl;
-      rewrite = target;
-    }
-  }
-
+  // Write the output
   if (output.value().size()) {
     ofstream ofs(output.value());
     ofs << rewrite.get_function() << endl;
