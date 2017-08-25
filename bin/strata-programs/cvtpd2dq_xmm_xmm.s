f741821a196f0ea670fc7f0f4fd5092ca0233905
Stefan Heule <stefanheule@gmail.com>
2017-08-25 13:11:55 -0700

    fix problems in strata formulas after parser fixes

--- bin/strata-programs/cvtpd2dq_xmm_xmm.s
+++ bin/strata-programs/cvtpd2dq_xmm_xmm.s
@@ -8,7 +8,7 @@
 
 # Text                     #  Line  RIP  Bytes  Opcode                  
 .target:                   #        0    0      OPC=<label>             
-  vcvtpd2dqx %xmm2, %xmm9  #  1     0    4      OPC=vcvtpd2dqx_xmm_xmm  
+  vcvtpd2dq %xmm2, %xmm9   #  1     0    4      OPC=vcvtpd2dq_xmm_xmm  
   movapd %xmm9, %xmm1      #  2     0x4  5      OPC=movapd_xmm_xmm      
   retq                     #  3     0x9  1      OPC=retq                
                                                                         
