9ce4c43c00ba091a762a494716827126b3e9a3d8
Berkeley Churchill <berkc@stanford.edu>
2018-01-23 16:00:51 -0800

    updating PATH in test.sh

--- popl19/test.sh
+++ popl19/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PATH=$PATH:$DIR/../bin
+
 for BENCHMARK in "example"; do
   echo "BENCHMARK $BENCHMARK"
   cd $DIR/$BENCHMARK
