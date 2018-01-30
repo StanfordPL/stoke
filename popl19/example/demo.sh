78181a81e035277d778716b9a32fdd22aeabc555
Berkeley Churchill <berkc@stanford.edu>
2018-01-29 20:26:35 -0800

    cleaning up debug tools

--- popl19/example/demo.sh
+++ popl19/example/demo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-stoke_debug_verify --target opt1/simple.s --rewrite opt1/simple_vec.s --strategy ddec --heap_out --stack_out --live_out "{ %rax }" --def_in "{ %rdi %rsi }" --solver z3 --alias_strategy flat --testcases testcases 
+stoke_debug_verify --target opt1/simple.s --rewrite opt1/simple_vec.s --strategy ddec --heap_out --stack_out --live_out "{ %rax }" --def_in "{ %rdi %rsi }" --solver z3 --alias_strategy flat --testcases testcases  --test_set "{ 1 .. 1800 }"
