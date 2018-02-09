7d99f30063089727e70039f4f42281b355b4788f
Berkeley Churchill <berkc@stanford.edu>
2018-02-08 18:06:06 -0800

    updated test script

--- popl19/test.sh
+++ popl19/test.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$PATH:$DIR/../bin
 
-for BENCHMARK in "example"; do
+for BENCHMARK in "example" "fibonacci"; do
   echo "BENCHMARK $BENCHMARK"
   cd $DIR/$BENCHMARK
   ./test.sh
