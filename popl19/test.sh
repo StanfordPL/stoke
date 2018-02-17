39b77649e2f5bcdb2111694426d9a656d0b3e97e
Berkeley Churchill <berkc@stanford.edu>
2018-02-17 13:47:03 -0800

    working strlen-unroll2 example

--- popl19/test.sh
+++ popl19/test.sh
@@ -3,7 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$PATH:$DIR/../bin
 
-for BENCHMARK in "example" "fibonacci" "fibonacci_modk" "fibonacci_peel_unroll"; do
+for BENCHMARK in "example"  \
+                 "fibonacci"  \
+                 "fibonacci_modk" \
+                 "fibonacci_peel_unroll" \
+                 "fibonacci_peel_unroll_modk"; do
   echo "BENCHMARK $BENCHMARK"
   cd $DIR/$BENCHMARK
   ./test.sh
