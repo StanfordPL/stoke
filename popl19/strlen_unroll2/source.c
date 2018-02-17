0e994867545116e9a14c88c6b1e88b222290a069
Berkeley Churchill <berkc@stanford.edu>
2018-02-17 13:47:03 -0800

    working strlen-unroll2 example

---
+++ popl19/strlen_unroll2/source.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define VECTORIZE 4
+
+/** I'm pretty sure this vectorized implementation is correct but
+  don't have a complete proof.  Used the bounded validator to find and fix some
+  overflow bugs, hopefully there's nothing left. */
+uint64_t 
+__attribute__ ((noinline))
+stlen_unroll(char* s) {
+
+  uint64_t len = 0;
+  while(*s++) {
+    len++;
+
+    if(*s++) {
+      len++;
+    } else {
+      break;
+    }
+  }
+
+  return len;
+}
+
+uint64_t 
+__attribute__ ((noinline))
+stlen(char* s) {
+
+  uint64_t len = 0;
+  while(*s++) {
+    len++;
+  }
+
+  return len;
+}
+
+int main (int argc, char* argv[])
+{
+  srand(0);
+
+  return 0;
+}
+
