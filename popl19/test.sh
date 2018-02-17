486c9d66d5c672f6eb06b894b72397faf7c5bb29
Berkeley Churchill <berkc@stanford.edu>
2018-02-17 13:25:54 -0800

    added fibonacci_peel_unroll benchmark

--- popl19/test.sh
+++ popl19/test.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$PATH:$DIR/../bin
 
-for BENCHMARK in "example" "fibonacci" "fibonacci_modk"; do
+for BENCHMARK in "example" "fibonacci" "fibonacci_modk" "fibonacci_peel_unroll"; do
   echo "BENCHMARK $BENCHMARK"
   cd $DIR/$BENCHMARK
   ./test.sh
