524296d741c67b6f805f3ffa240344e1a6885f1e
Berkeley Churchill <berkc@stanford.edu>
2018-02-26 20:37:30 -0800

    adding example2/example 4 replacing example; misc. files

---
+++ popl19/fibonacci_sums/demo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+stoke_debug_verify --target opt2/fib.s --rewrite opt2/fib_unroll.s --strategy ddec --heap_out --stack_out --live_out "{ %rax }" --def_in "{ %rdi %rsi }" --solver z3 --alias_strategy flat --testcases testcases --ddec_threads 2
