56b6f5a7e00bc66b9f3ea6166418dbd115f9c6d4
Berkeley Churchill <berkc@stanford.edu>
2018-04-03 22:11:08 -0700

    cleaning up strlen folder

---
+++ popl19/strlen/demo-z3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/usr/bin/time -o demo-z3.time -v stoke_debug_verify --strategy ddec --target opt1/func_2.s --rewrite opt1/func_1.s --testcases testcases --heap_out --stack_out --live_out "{ %rax }" --def_in "{ %rdi }" --solver z3 --alias_strategy arms_race --ddec_threads 6 --target_bound 9
+
