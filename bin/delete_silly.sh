a2edb8bb0595bfcb82089f2940b6a6da7d213ce8
Berkeley Churchill <berkc@stanford.edu>
2018-10-12 15:18:24 -0700

    removing extra files

--- bin/delete_silly.sh
+++ bin/delete_silly.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-psql -d v1 -U postgres -h 127.0.0.1 -c "delete from proofobligationqueue where hash in (select distinct Q.hash from proofobligationresult as R, proofobligationqueue as Q where R.hash = Q.hash and error is null)"
+
+while [ 1 ]; do
+  date
+  psql -d v1 -U postgres -h 127.0.0.1 -c "delete from proofobligationqueue where hash in (select distinct Q.hash from proofobligationresult as R, proofobligationqueue as Q where R.hash = Q.hash and error is null)"
+  sleep 30
+done
