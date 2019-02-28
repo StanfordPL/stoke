1737d86c50253ece6340e43f4f24519901b0cedb
Berkeley Churchill <berkc@stanford.edu>
2019-02-28 03:34:57 -0800

    hopefully speeding up build?

--- docker/user-setup.sh
+++ docker/user-setup.sh
@@ -2,17 +2,6 @@
 
 # fix PATH
 echo "PATH=\"\$HOME/stoke/bin:\$PATH\"" >> .bashrc
-echo "PATH=\"\$HOME/SageMath:\$PATH\"" >> .bashrc
-
-# download sagemath and unpack
-FILENAME=sage-8.5-Ubuntu_14.04-x86_64.tar.bz2
-
-wget --no-verbose http://mirrors.xmission.com/sage/linux/64bit/$FILENAME
-tar -xf $FILENAME
-rm $FILENAME
-
-# prepare sage
-echo "exit" | /home/stoke/SageMath/sage
 
 # download stoke
 git clone https://github.com/StanfordPL/stoke.git stoke
